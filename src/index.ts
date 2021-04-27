2e565e57b98120c43a6050d3b5d24d94d0a10292
noahvarghese <varghese.noah@gmail.com>
2021-04-27 00:34:16 -0400

    got the base server up and running

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import enableMultiThreading, {
 } from "./util/multithreading";
 import setupServer from "./util/server";
 
+// Run the server
 (async () => {
     if (enableMultiThreading) {
         setupMultiThreading();
--- src/util/server.ts
+++ src/util/server.ts
@@ -1,23 +1,51 @@
-import cors from "cors";
+import cors from "./cors";
+import cookieParser from "cookie-parser";
+import cluster from "cluster";
 import express from "express";
+import Logs, { LogLevels } from "./logs";
+import middlewares from "../middleware";
+import router from "../routes";
+import { createSession } from "./session";
+
+const port = process.env.PORT ?? 8080;
 
 const setupServer = async (): Promise<void> => {
     const app = express();
 
+    /* Disables header so clients don't know what server is hosting the site */
     app.disable("x-powered-by");
 
+    /* Configure the formats to receive information */
     app.use(express.json());
-
     app.use(express.urlencoded({ extended: true }));
 
-    app.use(cors({ origin: "*", credentials: true }));
+    /* Necessary for login and session fucntionality */
+    app.use(cookieParser());
 
-    app.use("/", (_, res) => {
-        res.send("HELLO");
-    });
+    /* Setup session */
+    app.use(await createSession());
 
-    app.listen(8080, () => {
-        console.log("SERVER STARTED ON PORT 8080");
+    /* Configure CORS */
+    app.use(cors);
+
+    /* Link all middlewares */
+    app.use(...middlewares);
+
+    /* Link all routes */
+    app.use("/", router);
+
+    /* Start the application already!!! */
+    app.listen(port, () => {
+        Logs.addLog(
+            `Server started on port: ${port} using ${
+                cluster.isMaster
+                    ? "parent process"
+                    : cluster.isWorker
+                    ? `child process ${cluster.worker.process.pid}`
+                    : undefined
+            }`,
+            LogLevels.EVENT
+        );
     });
 };
 
