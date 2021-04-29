67c96581b439ba944eec0d0a38245d7142e64bf6
noahvarghese <varghese.noah@gmail.com>
2021-04-29 17:59:47 -0400

    fixed squiggly

--- test/hooks/model.ts
+++ test/hooks/model.ts
@@ -5,6 +5,7 @@ import BaseModel from "../../src/models/abstract/base_model";
 import Business from "../../src/models/business";
 import BaseWorld from "../support/base_world";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const deleteModel = async <T extends BaseModel>(that: BaseWorld, type: any) => {
     const model = that.getCustomProp<T>("model");
     const connection = that.getCustomProp<Connection>("connection");
