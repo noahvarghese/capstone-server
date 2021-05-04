a6d0b37fbe2c701e425dbc2ef8b92a1e3ce55922
noahvarghese <varghese.noah@gmail.com>
2021-05-03 22:09:53 -0400

    another one

---
+++ src/models/role.test.ts
@@ -0,0 +1,141 @@
+import {
+    businessAttributes,
+    departmentAttributes,
+    permissionAttributes,
+    roleAttributes,
+    userAttributes,
+} from "../util/test/attributes";
+import BaseWorld from "../util/test/base_world";
+import DBConnection from "../util/test/db_connection";
+import { createModel, deleteModel } from "../util/test/model_actions";
+import {
+    testCreateModel,
+    testDeleteModel,
+    testReadModel,
+    testUpdateModel,
+} from "../util/test/model_compare";
+import Business, { BusinessAttributes } from "./business";
+import Department, { DepartmentAttributes } from "./department";
+import Permission, { PermissionAttributes } from "./permission";
+import Role, { RoleAttributes } from "./role";
+import User, { UserAttributes } from "./user/user";
+
+let baseWorld: BaseWorld | undefined;
+const key = "role";
+
+// Database setup
+beforeAll(DBConnection.InitConnection);
+afterAll(DBConnection.CloseConnection);
+
+// State Setup
+beforeEach(async () => {
+    baseWorld = new BaseWorld(await DBConnection.GetConnection());
+    baseWorld.setCustomProp<BusinessAttributes>(
+        "businessAttributes",
+        businessAttributes
+    );
+    baseWorld.setCustomProp<UserAttributes>("userAttributes", userAttributes);
+    baseWorld.setCustomProp<PermissionAttributes>(
+        "permissionAttributes",
+        permissionAttributes
+    );
+    baseWorld.setCustomProp<DepartmentAttributes>(
+        "departmentAttributes",
+        departmentAttributes
+    );
+    baseWorld.setCustomProp<RoleAttributes>("roleAttributes", roleAttributes);
+});
+afterEach(() => {
+    baseWorld = undefined;
+});
+
+// Domain setup
+beforeEach(async () => {
+    if (!baseWorld) {
+        throw new Error(BaseWorld.errorMessage);
+    }
+
+    const business = await createModel<Business, BusinessAttributes>(
+        baseWorld,
+        Business,
+        "business"
+    );
+
+    baseWorld.setCustomProp<UserAttributes>("userAttributes", {
+        ...baseWorld.getCustomProp<UserAttributes>("userAttributes"),
+        business_id: business.id,
+    });
+
+    const user = await createModel<User, UserAttributes>(
+        baseWorld,
+        User,
+        "user"
+    );
+
+    baseWorld.setCustomProp<DepartmentAttributes>("departmentAttributes", {
+        ...baseWorld.getCustomProp<DepartmentAttributes>(
+            "departmentAttributes"
+        ),
+        business_id: business.id,
+        updated_by_user_id: user.id,
+    });
+
+    const department = await createModel<Department, DepartmentAttributes>(
+        baseWorld,
+        Department,
+        "department"
+    );
+
+    baseWorld.setCustomProp<PermissionAttributes>("permissionAttributes", {
+        ...baseWorld.getCustomProp<PermissionAttributes>(
+            "permissionAttributes"
+        ),
+        updated_by_user_id: user.id,
+    });
+
+    const permission = await createModel<Permission, PermissionAttributes>(
+        baseWorld,
+        Permission,
+        "permission"
+    );
+
+    baseWorld.setCustomProp<RoleAttributes>("roleAttributes", {
+        ...baseWorld.getCustomProp<RoleAttributes>("roleAttributes"),
+        updated_by_user_id: user.id,
+        permission_id: permission.id,
+        department_id: department.id,
+    });
+});
+afterEach(async () => {
+    if (!baseWorld) {
+        throw new Error(BaseWorld.errorMessage);
+    }
+
+    await deleteModel<Permission>(baseWorld, Permission, "permission");
+    await deleteModel<Department>(baseWorld, Department, "department");
+    await deleteModel<User>(baseWorld, User, "user");
+    await deleteModel<Business>(baseWorld, Business, "business");
+});
+
+// Tests
+test("Create Role", async () => {
+    await testCreateModel<Role, RoleAttributes>(baseWorld, Role, key);
+});
+
+test("Update Role", async () => {
+    await testUpdateModel<Role, RoleAttributes>(
+        baseWorld,
+        Role,
+        key,
+        "name",
+        "TEST"
+    );
+});
+
+test("Delete Role", async () => {
+    await testDeleteModel<Role, RoleAttributes>(baseWorld, Role, key, "id");
+});
+
+test("Read Role", async () => {
+    await testReadModel<Role, RoleAttributes>(baseWorld, Role, key, "id");
+});
