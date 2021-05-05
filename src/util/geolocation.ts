5654299143c9c5f616caf78e4b3801b78ba743bb
noahvarghese <varghese.noah@gmail.com>
2021-05-05 02:21:04 +0000

    added pretty crappy excuse for a test, but at least it'll make sure it works

--- src/util/geolocation.ts
+++ src/util/geolocation.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
 
 interface HereApiParams {
     app_id: string;
@@ -8,7 +10,7 @@ interface HereApiParams {
     searchtext?: string;
 }
 
-interface LocationParams {
+export interface LocationParams {
     street: string;
     city: string;
     postal_code: string;
@@ -17,48 +19,37 @@ interface LocationParams {
 }
 
 export const getLocationData = async (
-    options: LocationParams = {
-        street: "1245 Sixth Line",
-        city: "Oakville",
-        postal_code: "L6H 1X1",
-        province: "ON",
-        country: "CA",
-    }
+    options: LocationParams
 ): Promise<any> => {
+    // Postal code messes up the query
+    // The returned values are only the first 3 digits
+    // So not optimal to search by
     const params: HereApiParams = {
         app_id: process.env.HERE_APP_ID ?? "",
         app_code: process.env.HERE_ACCESS_KEY_ID ?? "",
-        // Postal code messes up the query
-        // The returned values are only the first 3 digits
-        // So not optimal to search by
         searchtext: `${options.street} ${options.city} ${options.province} ${options.country}`,
     };
 
     const url = `https://autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json?apiKey=${
         process.env.HERE_API_KEY ?? ""
     }&query=${encodeURIComponent(params.searchtext ?? "")}`;
-    console.log(url);
 
     const res = await fetch(url);
-    // const body = await res.text();
 
-    // console.log(body);
     const data = await res.json();
-    // const view = data.Response.View;
-    // Logs.Log(view);
+
     return data.suggestions;
-    // return view;
 };
 
 // https://developer.here.com/blog/street-address-validation-with-reactjs-and-here-geocoder-autocomplete
-export const getFirstLocation = async (
-    options: LocationParams
-): Promise<any> => {
+export const getLocation = async (options: LocationParams): Promise<any> => {
     const locations = await getLocationData(options);
+
     if (locations.length > 1) {
         const loc = locations.find(
             (location: any) => location.matchLevel === "houseNumber"
         );
+
         return loc.address;
     } else if (locations.length > 0) {
         return locations[0].address;
