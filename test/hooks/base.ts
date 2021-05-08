c60ef4d8abce2db34d0630b9303b34e95c0cf6c7
noahvarghese <varghese.noah@gmail.com>
2021-05-08 18:46:56 +0000

    hooks

--- test/hooks/base.ts
+++ test/hooks/base.ts
@@ -1,7 +1,3 @@
 import { Before } from "@cucumber/cucumber";
 import { Connection, getConnection } from "typeorm";
 import BaseWorld from "../support/base_world";
-
-Before({ tags: "@Model" }, async function (this: BaseWorld) {
-    this.setCustomProp<Connection>("connection", getConnection());
-});
