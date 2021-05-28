c8a96eccef11175999e32947fe4e34299687184f
noahvarghese <varghese.noah@gmail.com>
2021-05-28 02:15:50 +0000

    abstracted out seperate fucntions

--- database/bin/reset_db.py
+++ database/bin/reset_db.py
@@ -18,10 +18,10 @@ help_menu = ("Exactly one argument must be specified\n\n" +
             "test\t--test/-t\tSelects the QA environment\n" +
             "help\t--help/-h\tPrints this menu\n")
 
-def main():
-    # get env to use from cli args
-    env = ""
+sql = []
 
+def get_env():
+    env = "" 
     try:
         opts, _ = getopt.getopt(sys.argv[1:], "dpt")
     except:
@@ -45,6 +45,81 @@ def main():
         else:
             print(help_menu)
             sys.exit()
+    
+    return env
+
+def read_file(file_name):
+    path = abspath(os.path.join(__file__, "../..", file_name))
+
+    with open(path) as file:
+        data = file.read()
+        
+    return data
+
+def format_file(data, dbname):
+    data = data.replace("${DATABASE}", dbname)
+    data = data.replace("\n", " ")
+
+    # Remove comments from the string
+    comment_regex = r"/\/*.*?\*\/"
+    data = re.sub(comment_regex, '', data)
+
+    return data
+
+def parse_delimited_sql(data):
+    # Keep these for secondary parsing
+    delimiter_changed_regex = r"DELIMITER\s\/\/.*?DELIMITER\s\;"
+
+    # Get and then remove the matches
+    delimiter_items = re.findall(delimiter_changed_regex, data, flags=re.DOTALL+re.MULTILINE)
+    data = re.sub(delimiter_changed_regex, '', data)
+
+    # get indexes of '//'
+    for item in delimiter_items:
+        prev_index = 0
+
+        # Seperate based off of index
+        while prev_index < len(item) - 1:
+            index = item.find("//", prev_index)
+
+            if ( index == -1 ):
+                next_index = len(item)
+            else:
+                next_index = index + 2
+
+            command = item[prev_index:next_index]
+            command = command.replace("//", '')
+                
+            if ( "DELIMITER" not in command):
+                sql.append(command)
+
+            prev_index = next_index + 1
+    
+    return data
+
+def parse_sql(data): 
+    # Split by ';'
+    sql_regex = r".*?\;"
+
+    matches = re.findall(sql_regex, data, flags=re.DOTALL+re.MULTILINE)
+
+    for statement in matches:
+        sql.append(statement)
+
+def execute_sql(db, cursor): 
+    for statment in sql:
+        try:
+            cursor.execute(statment)
+            db.commit()
+        except:
+            print("SQL query failed")
+            print(statment, "\n")
+            sys.exit()
+
+def main():
+    # get env to use from cli args
+
+    env = get_env()
 
     db = mysql.connector.connect(
         host = os.getenv("DB_URL"),
@@ -53,74 +128,25 @@ def main():
     )
 
     dbname = os.getenv("DB") + env
-    print(dbname)
 
     cursor = db.cursor()
-
-    sql = []
 
     for file in files:
         # Open schema.sql
         # Read into string
+        data = read_file(file)
+
         # remove newlines
-        path = abspath(os.path.join(__file__, "../..", file))
-
-        with open(path) as file:
-            data = file.read().replace('\n', ' ')
-
         # Replace all occurences of '${DATABASE}' with dbname + env
-        data = data.replace("${DATABASE}", dbname)
+        data = format_file(data, dbname)
 
-        # Remove these from the string
-        comment_regex = r"/\/*.*?\*\/"
-        data = re.sub(comment_regex, '', data)
+        data = parse_delimited_sql(data)
 
-        # Keep these for secondary parsing
-        delimiter_changed_regex = r"DELIMITER\s\/\/.*?DELIMITER\s\;"
+        data = parse_sql(data)
 
-        # Get and then remove the matches
-        delimiter_items = re.findall(delimiter_changed_regex, data, flags=re.DOTALL+re.MULTILINE)
-        data = re.sub(delimiter_changed_regex, '', data)
+        execute_sql(db, cursor)
 
-        # get indexes of '//'
-        for item in delimiter_items:
-            prev_index = 0
+        
 
-            # Seperate based off of index
-            while prev_index < len(item) - 1:
-                index = item.find("//", prev_index)
-
-                if ( index == -1 ):
-                    next_index = len(item)
-                else:
-                    next_index = index + 2
-
-                command = item[prev_index:next_index]
-                command = command.replace("//", '')
-                
-                if ( "DELIMITER" not in command):
-                    sql.append(command)
-
-                print(command)
-                
-                prev_index = next_index + 1
-
-
-        # Split by ';'
-        sql_regex = r".*?\;"
-
-        matches = re.findall(sql_regex, data, flags=re.DOTALL+re.MULTILINE)
-
-        for statement in matches:
-            sql.append(statement)
-
-        for statment in sql:
-            try:
-                cursor.execute(statment)
-                db.commit()
-            except:
-                print("SQL query failed")
-                print(statment, "\n")
-                sys.exit()
 
 main()
