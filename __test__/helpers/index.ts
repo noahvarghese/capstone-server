7289e7e18e6f05313ea5ee4f0b5ea85853c85f79
noahvarghese <varghese.noah@gmail.com>
2021-10-28 22:49:37 +0000

    forgot to remove reference to deleted file

--- __test__/helpers/index.ts
+++ __test__/helpers/index.ts
@@ -1,6 +1,5 @@
 import Api from "./api";
-import Database from "./database";
 import AppServer from "./server";
 import Model from "./model";
 
-export default { Api, Database, AppServer, Model };
+export default { Api, AppServer, Model };
