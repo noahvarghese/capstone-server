88fd3c47ed89e40f8c4a9b18095972f392f6fd27
noahvarghese <varghese.noah@gmail.com>
2021-10-30 18:24:59 +0000

    removed encasing quotes from version

--- bin/files/get_version_from_package_json.sh
+++ bin/files/get_version_from_package_json.sh
@@ -6,7 +6,7 @@ VERSION=$(cat ./package.json | grep version | cut -d : -f 2 | sed 's/\",$//g' | 
 TRAILING_VERSION=$(echo $VERSION | cut -d . -f 3)
 # Adds 1 to last digit of version number
 TRAILING_VERSION=$((TRAILING_VERSION+1))
-VERSION=\"$(echo $VERSION | cut -d . -f 1).$(echo $VERSION | cut -d . -f 2).$TRAILING_VERSION\"
+VERSION=$(echo $VERSION | cut -d . -f 1).$(echo $VERSION | cut -d . -f 2).$TRAILING_VERSION
 
 echo $VERSION
 
