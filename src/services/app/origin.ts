485aeda3721a96fb46e44a7b03654da3462d8ce1
noahvarghese <varghese.noah@gmail.com>
2021-11-08 16:48:39 +0000

    added debugging forr cors

--- src/services/app/origin.ts
+++ src/services/app/origin.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import { client } from "@util/permalink";
+import Logs from "@util/logs/logs";
 
 let origin = client();
 
 if (origin[origin.length - 1] === "/") {
     origin = origin.substring(0, origin.length - 1);
 }
+
+Logs.Debug(origin, origin + "/");
 
 const corsOptions = {
     origin: [origin, origin + "/"],
