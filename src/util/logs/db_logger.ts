2cfcffda8a215617a736957d71cbac4958557494
noahvarghese <varghese.noah@gmail.com>
2021-12-14 02:41:45 +0000

    utilized new level of logging

--- src/util/logs/db_logger.ts
+++ src/util/logs/db_logger.ts
@@ -1,8 +1,3 @@
-/*
-“StAuth10065: I Noah Varghese, 000753196 certify that this material is my original work. 
-No other person’s work has been used without due acknowledgement. 
-I have not made my work available to anyone else.”
-*/
 import { Logger, QueryRunner } from "typeorm";
 import Logs from "./logs";
 
@@ -10,7 +5,7 @@ export default class DBLogger implements Logger {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     logQuery(query: string, parameters?: unknown[], _?: QueryRunner): void {
         const message = `Query executing: ${query}\nParams: ${parameters}`;
-        Logs.SQL(message);
+        Logs.DbLogger(message);
     }
     logQueryError(
         error: string | Error,
@@ -32,11 +27,11 @@ export default class DBLogger implements Logger {
     }
     logSchemaBuild(contents: string, queryRunner?: QueryRunner): void {
         const message = `Schema being built: ${contents}\nQuery Runner: ${queryRunner}`;
-        Logs.SQL(message);
+        Logs.DbLogger(message);
     }
     logMigration(contents: string, queryRunner?: QueryRunner): void {
         const message = `Migration being ran: ${contents}\nQuery Runner: ${queryRunner}`;
-        Logs.SQL(message);
+        Logs.DbLogger(message);
     }
     log(
         level: "log" | "info" | "warn",
@@ -44,6 +39,6 @@ export default class DBLogger implements Logger {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         _?: QueryRunner
     ): void {
-        Logs.SQL(`[${level.toUpperCase()}]`, message);
+        Logs.DbLogger(`[${level.toUpperCase()}]`, message);
     }
 }
