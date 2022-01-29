e21027759b6b38880f49d6e7ec273b9bfcad6259
noahvarghese <varghese.noah@gmail.com>
2022-01-29 17:02:14 +0000

    added script to load env

---
+++ bin/env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+envup() {
+  local file=$([ -z "$1" ] && echo ".env" || echo ".env.$1")
+
+  if [ -f $file ]; then
+    echo "Loading environment variables"
+    set -a
+    source $file
+    set +a
+  else
+    echo "No $file file found" 1>&2
+    return 1
+  fi
+}
+
+# envup "$@"
+# Run as source ./bin/env
