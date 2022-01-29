6e331ba293893e061a37cc8c2edd84c313bebde9
noahvarghese <varghese.noah@gmail.com>
2022-01-29 19:25:11 +0000

    use custom test reporter and cleanup

--- bin/test.sh
+++ bin/test.sh
@@ -3,14 +3,8 @@
 BRANCH_NAME=$(git branch --show-current)
 BRANCH_NAME=$(echo $BRANCH_NAME | sed -r 's/[-.:]/_/g')
 
-# DYNAMIC NAME
-export DB_ENV=test_"$BRANCH_NAME"_$(date +%s)
-DB_NAME="${DB_NAME}${DB_ENV}"
-
-echo "[ Event ]: Initting ${DB_NAME}"
-
-# Setup database
-npm run database:reset:ci -- $DB_ENV
+# Database name extension
+EXTENSION="$BRANCH_NAME"_$(date +%s)
 
 # Enable ** glob for coverage report
 command -v shopt &>/dev/null
@@ -18,18 +12,24 @@ shopt -s globstar
 
 NODE_ENV=test
 
-./node_modules/.bin/jest --runInBand "$@" ;
+# Setup database
+npm run database:reset:test -- $EXTENSION
+
+# Custom test reporting
+./node_modules/.bin/nyc --silent --no-clean ./node_modules/.bin/jest --runInBand "$@" ;
 
 TEST_RESULT=$?
 
 if [ $TEST_RESULT -gt 0 ]; then
     # If the database is empty then drop the database
-    npm run database:is_empty -- $DB_ENV --tables business,user
+    npm run database:is_empty:test -- $EXTENSION --tables business,user
 
     if [ $? -eq 0 ]; then
-        npm run database:drop $DB_ENV
+        npm run database:drop:test $EXTENSION
     fi
+
     exit 1
 else
+    # If the tests passed then the database should be empty
     npm run database:drop $DB_ENV
 fi
