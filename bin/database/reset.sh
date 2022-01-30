5c3c0334abeff86fc9adf55ea1c182dedbede96d
noahvarghese <varghese.noah@gmail.com>
2022-01-30 17:30:08 +0000

    got reset and drop shell scripts complete

--- bin/database/reset.sh
+++ bin/database/reset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source ./bin/database/lib/ext.sh
 source ./bin/database/lib/sql.sh
 source ./bin/env.sh
 
 reset_db() {
     EXTENSION=$1
+    DIR=$2
 
     DB="${DB_NAME}${EXTENSION}"
 
@@ -15,7 +15,7 @@ reset_db() {
     exec_sql $SQL_INIT_FILE
     rm $SQL_INIT_FILE
 
-    readarray -d '' entries < <(printf '%s\0' database/*.sql | sort -zV)
+    readarray -d '' entries < <(printf '%s\0' "$DIR"/*.sql | sort -zV)
 
     for entry in "${entries[@]}"; do
         exec_sql $DB "${entry}"
@@ -25,16 +25,93 @@ reset_db() {
     done
 }
 
+read -r -d '' HELP<<EOF
+    [ HELP ]: This script resets a database given a series of environment variables, SQL files, and name extensions\n
+    --sql-dir\tdirectory in which to find the sql files to execute, executes them alphabetically\n
+    [--env-path]\toptional path to a file that has the environment variables, this is only used if the NODE_ENV variable is set to 'dev'\n
+    Requires one of the next three (3) options\n
+    \t-t[=]\tappends '_test' to the database name, if a value is provided (must be no spaces between flag and value) it is appended as '_test_value'\n
+    \t-d\tappends '_dev' to the database name\n
+    \t-p\tdoes not append anything to the database name\n
+    Environment variables:\n
+    \tDB_NAME\tthe name of the database\n
+    \tDB_USER\tthe user to be logged in as\n
+    \tDB_PWD\tthe password to login with\n
+    \tDB_URL\tthe hostname/FQDN/IP address of the database
+EOF
+
+help() {
+    echo -e $HELP 1>&2
+    exit $1
+}
+
+DB_ENV=
+ENV=
+DIR=
+
+TEMP=$(getopt -o 'dpt::' --long 'sql-dir:,env-path:' -n 'reset.sh' -- "$@")
+
+if [ $? -ne 0 ]; then
+	echo 'Terminating...' >&2
+	exit 1
+fi
+
+eval set -- "$TEMP"
+unset TEMP
+
+while true; do
+    case "$1" in
+        '-t')  
+            case "$2" in
+                '')
+                    DB_ENV="_test"
+                ;;
+                *)
+                    DB_ENV="_test_$2"
+                ;;
+            esac
+            shift 2
+            continue
+        ;;
+        '-d')  
+            DB_ENV="_dev" 
+            shift
+            continue
+        ;;
+        '-p')  
+            shift
+            continue
+        ;;
+        '--sql-dir')
+            DIR="$2"
+            shift 2
+            continue
+        ;;
+        '--env-path')
+            ENV="$2"
+            shift 2
+            continue
+        ;;
+        '-h'|'--help') 
+            help 0 
+        ;;
+        '--')
+            shift
+            break
+        ;;
+        *) 
+            help 1
+        ;;
+    esac
+done 
+
 # setup environment locally as CI and prod should have all variables already loaded
 if [[ $NODE_ENV == 'dev' ]]; then
-    if ! envup $(env_path "$@"); then
+    if ! envup $ENV; then
         exit 1
     fi
+else
+    echo "[ INFO ]: Operating within Prod or CI environment, variables will not be loaded, run `export NODE_ENV=dev` to enable --env-path"
 fi
 
-if [[ -z $1 ]]; then
-    echo "[ ERROR ]: Must provide a database name" 1>&2
-    exit 1
-fi
-
-reset_db $(get_db_ext "$@")
+reset_db "$DB_ENV" "$DIR"
--- bin/env.sh
+++ bin/env.sh
@@ -1,43 +1,19 @@
 #!/bin/bash
 
-read -r -d '' ENV_PATH_HELP<<EOF
-  \n[ ERROR ]: No file found to load environment variables .env, .env.$1, $1
-EOF
-
-env_path_help() {
-    echo -e $ENV_PATH_HELP 1>&2
-    exit 1
-}
-
-env_path() {
-    PATH=""
-    for ((i=1;i<=$#;i++)); do
-      case ${!i} in
-        "-p")
-          i=$((i+1))
-          PATH=${!i}  
-          echo $PATH
-          ;;
-        "--path")
-          i=$((i+1))
-          PATH=${!i}  
-          ;;
-      esac
-    done
-    echo $PATH
-}
-
 envup() {
-  local file=$([ -z "$1" ] && echo ".env" || echo ".env.$1")
+  # If no file provided use .env else use file name
+  local file=$([ -z "$1" ] && echo ".env" || echo "$1")
+  # Check if file provided is actually a file, otherwise use .env.$file
+  file=$([ -f "$file" ] && echo "$file" || echo ".env.$file")
 
+  # Check again if file is a file
   if [ -f $file ]; then
     echo "[ EVENT ]: Loading environment variables from $file"
     set -a
     source $file
     set +a
   else
-    echo "[ ERROR ]: No $file file found" 1>&2
-    echo "$(env_path_help $file)" 1>&2
+    echo "[ ERROR ]: No file -> $1, or $file found" 1>&2
     return 1
   fi
 }
