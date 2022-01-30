bafb4af0646da0fc348008214240a5529e83542f
noahvarghese <varghese.noah@gmail.com>
2022-01-30 23:57:47 +0000

    added test for updating permissions when role is locked

--- src/models/permission.db.test.ts
+++ src/models/permission.db.test.ts
@@ -3,6 +3,8 @@ import DBConnection from "@test/support/db_connection";
 import ModelTestPass from "@test/model/helpers/test/pass";
 import Permission, { PermissionAttributes } from "./permission";
 import Model from "@test/model/helpers";
+import Role, { RoleAttributes } from "./role";
+import ModelActions from "@test/model/helpers/actions";
 
 let baseWorld: BaseWorld;
 
@@ -10,48 +12,92 @@ let baseWorld: BaseWorld;
 beforeAll(DBConnection.init);
 afterAll(DBConnection.close);
 
-// State Setup
-beforeEach(async () => {
-    baseWorld = new BaseWorld(await DBConnection.get());
-    await Model.setup.call(baseWorld, Permission);
+describe("USing permission setup", () => {
+    // State Setup
+    beforeEach(async () => {
+        baseWorld = new BaseWorld(await DBConnection.get());
+        await Model.setup.call(baseWorld, Permission);
+    });
+
+    afterEach(async () => {
+        await Model.teardown.call(baseWorld, Permission);
+        baseWorld.resetProps();
+    });
+
+    // Tests
+    test("Create Permission", async () => {
+        await ModelTestPass.create<Permission, PermissionAttributes>(
+            baseWorld,
+            Permission
+        );
+    });
+
+    test("Update Permission", async () => {
+        await ModelTestPass.update<Permission, PermissionAttributes>(
+            baseWorld,
+            Permission,
+
+            { global_assign_resources_to_department: false }
+        );
+    });
+
+    test("Delete Permission", async () => {
+        await ModelTestPass.delete<Permission, PermissionAttributes>(
+            baseWorld,
+            Permission,
+
+            ["id"]
+        );
+    });
+
+    test("Read Permission", async () => {
+        await ModelTestPass.read<Permission, PermissionAttributes>(
+            baseWorld,
+            Permission,
+
+            ["id"]
+        );
+    });
 });
 
-afterEach(async () => {
-    await Model.teardown.call(baseWorld, Permission);
-    baseWorld.resetProps();
-});
+describe("Using role setup", () => {
+    // State Setup
+    beforeEach(async () => {
+        baseWorld = new BaseWorld(await DBConnection.get());
+        await Model.setup.call(baseWorld, Role);
+    });
 
-// Tests
-test("Create Permission", async () => {
-    await ModelTestPass.create<Permission, PermissionAttributes>(
-        baseWorld,
-        Permission
-    );
-});
+    afterEach(async () => {
+        await Model.teardown.call(baseWorld, Role);
+        baseWorld.resetProps();
+    });
 
-test("Update Permission", async () => {
-    await ModelTestPass.update<Permission, PermissionAttributes>(
-        baseWorld,
-        Permission,
+    test("Role edit lock prevents permissions from being edited", async () => {
+        if (!baseWorld) throw new Error(BaseWorld.errorMessage);
 
-        { global_assign_resources_to_department: false }
-    );
-});
+        baseWorld.setCustomProp<RoleAttributes>("roleAttributes", {
+            ...baseWorld.getCustomProp<RoleAttributes>("roleAttributes"),
+            prevent_edit: true,
+        });
 
-test("Delete Permission", async () => {
-    await ModelTestPass.delete<Permission, PermissionAttributes>(
-        baseWorld,
-        Permission,
+        await ModelActions.create<Role, RoleAttributes>(baseWorld, Role);
 
-        ["id"]
-    );
-});
+        let errorThrown = false;
 
-test("Read Permission", async () => {
-    await ModelTestPass.read<Permission, PermissionAttributes>(
-        baseWorld,
-        Permission,
+        try {
+            await ModelActions.update<Permission, PermissionAttributes>(
+                baseWorld,
+                Permission,
+                {}
+            );
+        } catch (e) {
+            errorThrown = true;
+            expect((e as Partial<{ message?: string }>).message).toMatch(
+                /PermissionUpdateError: Cannot edit permissions while edit lock is set/
+            );
+        }
 
-        ["id"]
-    );
+        await ModelActions.delete<Role>(baseWorld, Role);
+        expect(errorThrown).toBe(true);
+    });
 });
