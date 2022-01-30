c8233c3494ac6eca987c32368dcfa92e298322fd
noahvarghese <varghese.noah@gmail.com>
2022-01-30 18:50:54 +0000

    fixed string evaluation

--- bin/database/reset.sh
+++ bin/database/reset.sh
@@ -111,7 +111,7 @@ if [[ $NODE_ENV == 'dev' ]]; then
         exit 1
     fi
 else
-    echo "[ INFO ]: Operating within Prod or CI environment, variables will not be loaded, run `export NODE_ENV=dev` to enable --env-path"
+    echo "[ INFO ]: Operating within Prod or CI environment, variables will not be loaded, run 'export NODE_ENV=dev' to enable --env-path"
 fi
 
 reset_db "$DB_ENV" "$DIR"
--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BRANCH_NAME=$(git branch --show-current)
-BRANCH_NAME=$(echo $BRANCH_NAME | sed -r 's/[-.:]/_/g')
+branch_name=$(git branch --show-current)
+branch_name=$(echo $branch_name | sed -r 's/[-.:]/_/g')
 
 # Database name extension
-EXTENSION="$BRANCH_NAME"_$(date +%s)
+extension="$branch_name"_$(date +%s)
 
 # Enable ** glob for coverage report
 command -v shopt &>/dev/null
@@ -13,23 +13,23 @@ shopt -s globstar
 NODE_ENV=test
 
 # Setup database
-npm run database:reset:test -- $EXTENSION
+npm run database:reset -- "-t$extension"
 
 # Custom test reporting
 ./node_modules/.bin/nyc --silent --no-clean ./node_modules/.bin/jest --runInBand "$@" ;
 
-TEST_RESULT=$?
+test_result=$?
 
-if [ $TEST_RESULT -gt 0 ]; then
+if [ $test_result -gt 0 ]; then
     # If the database is empty then drop the database
-    npm run database:is_empty:test -- $EXTENSION --tables business,user
+    npm run database:is_empty -- "-t$extension"
 
     if [ $? -eq 0 ]; then
-        npm run database:drop:test $EXTENSION
+        npm run database:drop -- "-t$extension"
     fi
 
     exit 1
 else
     # If the tests passed then the database should be empty
-    npm run database:drop $DB_ENV
+    npm run database:drop -- "-t$extension"
 fi
