57930041c7b0b6d3bc2d72692b3764c93a8a452e
noahvarghese <varghese.noah@gmail.com>
2022-01-31 19:35:11 +0000

    added route settings to request

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,9 +1,17 @@
+import { PermissionAttributes } from "@models/permission";
 import { Connection } from "typeorm";
 
 declare global {
+    type RouteSettings = {
+        permissions: (keyof Omit<PermissionAttributes, "updated_by_user_id">)[];
+        passthrough: boolean;
+        requireAuth: boolean;
+    };
+
     namespace Express {
         interface Request {
-            SqlConnection: Connection;
+            routeSettings: RouteSettings;
+            sqlConnection: Connection;
         }
     }
 }
