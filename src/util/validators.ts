44001381bcfc4e8e4bff5d0fc2aed9f6145e3f23
noahvarghese <varghese.noah@gmail.com>
2022-02-03 19:49:05 +0000

    added functionality and tests for validator

--- src/util/validators.ts
+++ src/util/validators.ts
@@ -1,4 +1,7 @@
 import { PhoneNumber, PhoneNumberUtil } from "google-libphonenumber";
+import validator from "validator";
+import DataServiceError, { ServiceErrorReasons } from "./errors/service";
+import Logs from "./logs/logs";
 
 // Only canadian postal codes
 export const isPostalCode = (val: string): boolean =>
@@ -20,18 +23,97 @@ export const isPhone = (val: string, regionCode = "CA"): boolean => {
     }
 };
 
-export const emptyChecker = <T>(
-    obj: T
-): undefined | { message: string; field: string } => {
-    for (const [key, value] of Object.entries(obj)) {
-        if (!value || (value as string).trim() === "") {
-            return {
-                message: `${(key[0].toUpperCase() + key.substring(1))
-                    .split("_")
-                    .join(" ")} cannot be empty`,
-                field: key,
-            };
+type EmptyMapperKey =
+    | "string"
+    | "number"
+    | "undefined"
+    | "boolean"
+    | "object"
+    | "function"
+    | "bigint"
+    | "symbol";
+
+type EmptyMapper = {
+    [x in EmptyMapperKey]: {
+        validator: (v?: unknown) => boolean;
+    };
+};
+
+const empty: EmptyMapper = {
+    string: {
+        validator: (v: unknown) =>
+            !validator.isEmpty(v as string, { ignore_whitespace: true }),
+    },
+    number: {
+        validator: (v: unknown) => !isNaN(Number(v)),
+    },
+    undefined: {
+        validator: () => false,
+    },
+    boolean: {
+        validator: (v: unknown) =>
+            v !== undefined && (v === true || v === false),
+    },
+    function: {
+        validator: (v: unknown) => {
+            throw new Error(`Validator for function type not implemented ${v}`);
+        },
+    },
+    object: {
+        validator: (v: unknown) => {
+            if (v === null) return false;
+            const res =
+                v &&
+                Object.keys(v as Record<string, unknown>).length === 0 &&
+                Object.getPrototypeOf(v) === Object.prototype;
+            return !res;
+        },
+    },
+    bigint: {
+        validator: (v: unknown) => {
+            throw new Error(`Validator for bigint type not implemented ${v}`);
+        },
+    },
+    symbol: {
+        validator: (v: unknown) => {
+            throw new Error(`Validator for symbol type not implemented ${v}`);
+        },
+    },
+};
+
+type Expected = {
+    [x: string]: {
+        required: boolean;
+        value: unknown;
+    };
+};
+
+/**
+ * Throws error with details that can be used to generate a response
+ * @param obj
+ */
+export const emptyChecker = (
+    obj: Expected
+): undefined | { message: string; field: keyof typeof obj } => {
+    for (const key of Object.keys(obj)) {
+        const { value, required } = obj[key];
+        const validatorFn = empty[typeof value].validator;
+
+        try {
+            const valid = validatorFn(value);
+
+            if (required && !valid) {
+                const message = `${key.replace(
+                    "_",
+                    " "
+                )} cannot be empty, value -> ${value}, required -> ${required}`;
+                return { message, field: key };
+            }
+        } catch (_e) {
+            Logs.Error((_e as Error).message);
+            throw new DataServiceError(ServiceErrorReasons.UTILITY);
         }
     }
+
     return undefined;
 };
--- src/util/validators.unit.test.ts
+++ src/util/validators.unit.test.ts
@@ -1,21 +1,81 @@
-import { isPostalCode } from "./validators";
+import Logs from "./logs/logs";
+import { emptyChecker, isPostalCode } from "./validators";
 
-test("Postal code too short", () => {
-    const valid = isPostalCode("A");
-    expect(valid).toBe(false);
+describe("postal code", () => {
+    test("too short", () => {
+        const valid = isPostalCode("A");
+        expect(valid).toBe(false);
+    });
+
+    test("Incorrect order", () => {
+        const valid = isPostalCode("1A12B3");
+        expect(valid).toBe(false);
+    });
+
+    test("Valid", () => {
+        const valid = isPostalCode("L5V 2A4");
+        expect(valid).toBe(true);
+    });
+
+    test("No space should be valid", () => {
+        const valid = isPostalCode("L5V2A4");
+        expect(valid).toBe(true);
+    });
 });
 
-test("Incorrect order", () => {
-    const valid = isPostalCode("1A12B3");
-    expect(valid).toBe(false);
-});
+describe("empty checker", () => {
+    describe("valid options", () => {
+        const cases = [
+            { value: "hi", required: true },
+            { value: "hi", required: false },
+            { value: "", required: false },
+            { value: undefined, required: false },
+            { value: null, required: false },
+            { value: 1, required: true },
+            { value: 1, required: false },
+            { value: NaN, required: false },
+            { value: true, required: true },
+            { value: true, required: false },
+            { value: false, required: true },
+            { value: false, required: false },
+            { value: {}, required: false },
+            { value: { test: "123" }, required: true },
+            { value: { test: "123" }, required: false },
+            { value: { test: {} }, required: false },
+            { value: { test: {} }, required: true },
+        ];
 
-test("Valid", () => {
-    const valid = isPostalCode("L5V 2A4");
-    expect(valid).toBe(true);
-});
+        test.each(cases)("test %p", ({ value, required }) => {
+            const res = emptyChecker({ test: { value, required } });
+            if (res)
+                Logs.Error({
+                    expected: "undefined",
+                    received: "ERROR",
+                    ...res,
+                });
+            expect(res).toBe(undefined);
+        });
+    });
 
-test("No space should be valid", () => {
-    const valid = isPostalCode("L5V2A4");
-    expect(valid).toBe(true);
+    describe("invalid options", () => {
+        const cases = [
+            { value: "", required: true },
+            { value: undefined, required: true },
+            { value: null, required: true },
+            { value: NaN, required: true },
+            { value: {}, required: true },
+        ];
+
+        test.each(cases)("test %p", ({ value, required }) => {
+            const res = emptyChecker({ test: { value, required } });
+            if (!res)
+                Logs.Error({
+                    expected: "ERROR",
+                    received: "undefined",
+                    value,
+                    required,
+                });
+            expect(res).not.toBe(undefined);
+        });
+    });
 });
