3cfe2f0cf9ab3df6c968997656abe60051a5301c
noahvarghese <varghese.noah@gmail.com>
2022-02-08 18:03:39 +0000

    added test setup function

---
+++ __test__/unit/setup.ts
@@ -0,0 +1,39 @@
+import Business from "@models/business";
+import User from "@models/user/user";
+import { registerHandler } from "@routes/auth/register/handler";
+import { businessAttributes, userAttributes } from "@test/model/attributes";
+import { Connection } from "typeorm";
+
+/**
+ * Creates Business, User, Membership, Department, Permission, Role, and UserRole objects
+ * Sets up department and role as admin, with all permissions
+ * Prevent edit and delete of membership, department, role and user role objects as this user is the owner of the business
+ * @param conn database connection
+ * @returns business id (a positive integer) or NaN if not found
+ * @throws on database error
+ */
+export const setupAdmin = async (
+    conn: Connection
+): Promise<{ business_id: number; user_id: number }> => {
+    const b = businessAttributes();
+    const u = userAttributes();
+
+    await registerHandler(conn, {
+        address: b.address,
+        city: b.city,
+        confirm_password: u.password,
+        password: u.password,
+        name: b.name,
+        email: u.email,
+        first_name: u.first_name,
+        last_name: u.last_name,
+        phone: u.phone,
+        postal_code: b.postal_code,
+        province: b.province,
+    });
+
+    return {
+        business_id: (await conn.manager.findOne(Business))?.id ?? NaN,
+        user_id: (await conn.manager.findOne(User))?.id ?? NaN,
+    };
+};
