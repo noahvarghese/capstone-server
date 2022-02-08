14372e5863c7c93f1b28499389a4d41e651564eb
noahvarghese <varghese.noah@gmail.com>
2022-02-08 03:07:53 +0000

    Added teardown for unit tests

--- src/routes/members/get_controller.test.ts
+++ src/routes/members/get_controller.test.ts
@@ -1,7 +1,17 @@
+import DBConnection from "@test/support/db_connection";
+import { unitTeardown } from "@test/unit/teardown";
+
 test.todo("db connection failed");
 
 describe("requires db connection", () => {
+    beforeAll(DBConnection.init);
+    afterAll(DBConnection.close);
+
     describe("requires admin user", () => {
+        beforeAll(async () => {
+            return;
+        });
+        afterAll(async () => unitTeardown(await DBConnection.get()));
         describe("requires secondary user", () => {
             describe("permissions", () => {
                 test.todo("admin");
