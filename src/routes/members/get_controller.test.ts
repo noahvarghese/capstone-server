f8cbd0f23e1dd1a7c6a845892fa89875601f7b1e
noahvarghese <varghese.noah@gmail.com>
2022-02-08 18:06:24 +0000

    added all setup and teardown for tests

--- src/routes/members/get_controller.test.ts
+++ src/routes/members/get_controller.test.ts
@@ -1,4 +1,10 @@
+import Department from "@models/department";
+import Role from "@models/role";
+import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
+import { registerHandler } from "@routes/auth/register/handler";
 import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
 import { unitTeardown } from "@test/unit/teardown";
 
 test.todo("db connection failed");
@@ -8,11 +14,65 @@ describe("requires db connection", () => {
     afterAll(DBConnection.close);
 
     describe("requires admin user", () => {
+        let business_id: number, user_id: number;
+
         beforeAll(async () => {
-            return;
+            // Create admin user
+            ({ business_id, user_id } = await setupAdmin(
+                await DBConnection.get()
+            ));
         });
+
         afterAll(async () => unitTeardown(await DBConnection.get()));
+
         describe("requires secondary user", () => {
+            beforeAll(async () => {
+                // create secondary user/department/role/user role
+                const conn = await DBConnection.get();
+                const [
+                    {
+                        identifiers: [{ id: secondaryUserId }],
+                    },
+                    {
+                        identifiers: [{ id: department_id }],
+                    },
+                ] = await Promise.all([
+                    conn.manager.insert(
+                        User,
+                        new User({
+                            first_name: "test",
+                            last_name: "test",
+                            email: process.env.TEST_EMAIL_2 ?? "",
+                        })
+                    ),
+                    conn.manager.insert(
+                        Department,
+                        new Department({
+                            name: "test",
+                            business_id,
+                            updated_by_user_id: user_id,
+                        })
+                    ),
+                ]);
+
+                const {
+                    identifiers: [{ id: role_id }],
+                } = await conn.manager.insert(
+                    Role,
+                    new Role({
+                        updated_by_user_id: user_id,
+                        department_id,
+                        name: "test",
+                    })
+                );
+
+                await conn.manager.insert(
+                    UserRole,
+                    new UserRole({ user_id: secondaryUserId, role_id })
+                );
+                return;
+            });
+
             describe("permissions", () => {
                 test.todo("admin");
                 test.todo("department");
