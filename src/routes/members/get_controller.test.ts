0b0049f6c177910fa5243593e30c82d4e537e6fe
noahvarghese <varghese.noah@gmail.com>
2022-02-08 02:47:37 +0000

    added todos for tests

---
+++ src/routes/members/get_controller.test.ts
@@ -0,0 +1,30 @@
+test.todo("db connection failed");
+
+describe("requires db connection", () => {
+    describe("requires admin user", () => {
+        describe("requires secondary user", () => {
+            describe("permissions", () => {
+                test.todo("admin");
+                test.todo("department");
+                test.todo("none");
+            });
+
+            describe("pagination", () => {
+                test.todo("limit");
+                test.todo("page");
+            });
+
+            describe("filter", () => {
+                return;
+            });
+
+            describe("search", () => {
+                return;
+            });
+
+            describe("sort", () => {
+                return;
+            });
+        });
+    });
+});
