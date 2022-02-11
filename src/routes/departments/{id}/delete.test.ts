1865b81d443a2839f001c1c4e27a131fb79473a9
noahvarghese <varghese.noah@gmail.com>
2022-02-11 18:30:28 +0000

    added delete functionality

--- src/routes/departments/{id}/delete.test.ts
+++ src/routes/departments/{id}/delete.test.ts
@@ -1,3 +1,169 @@
+import { getMockRes } from "@jest-mock/express";
+import Department from "@models/department";
+import Role, { AccessKey } from "@models/role";
+import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { Request } from "express";
+import { Connection } from "typeorm";
+import deleteController from "./delete";
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
+let business_id: number, user_id: number;
+let conn: Connection;
+
+let id: number;
+const name = "TEST";
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+    ({ business_id, user_id } = await setupAdmin(await DBConnection.get()));
+
+    ({
+        identifiers: [{ id }],
+    } = await conn.manager.insert(
+        Department,
+        new Department({
+            name,
+            business_id,
+            updated_by_user_id: user_id,
+        })
+    ));
+});
+
+afterAll(async () => {
+    const conn = await DBConnection.get();
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+describe("prevent delete", () => {
+    beforeAll(async () => {
+        await conn.manager.update(
+            Department,
+            { id, business_id },
+            { prevent_delete: true }
+        );
+    });
+
+    afterAll(async () => {
+        await conn.manager.update(
+            Department,
+            { id, business_id },
+            { prevent_delete: false }
+        );
+    });
+
+    test("405", async () => {
+        await deleteController(
+            {
+                session: {
+                    user_id,
+                    current_business_id: business_id,
+                    business_ids: [business_id],
+                },
+                params: { id },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+
+        expect(res.sendStatus).toHaveBeenCalledWith(405);
+    });
+});
+
+describe("invalid id", () => {
+    const cases = [{ id: "YOLO" }, { new_id: 10000 }];
+    test.each(cases)("%p", async ({ new_id }) => {
+        await deleteController(
+            {
+                session: {
+                    user_id,
+                    business_ids: [business_id],
+                    current_business_id: business_id,
+                },
+                params: {
+                    id: new_id,
+                },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+        expect(res.sendStatus).toHaveBeenLastCalledWith(400);
+    });
+});
+
+test("bad db connection", async () => {
+    await deleteController(
+        {
+            session: { user_id, current_business_id: business_id },
+            body: { name: "TEst" },
+            params: {
+                id,
+            },
+        } as unknown as Request,
+        res
+    );
+    expect(res.sendStatus).toHaveBeenCalledWith(500);
+});
+
 describe("permissions", () => {
-    test.todo("");
+    const cases = [
+        { access: "ADMIN" },
+        { access: "MANAGER" },
+        { access: "USER" },
+    ];
+
+    describe.each(cases)("%p", ({ access }) => {
+        beforeEach(async () => {
+            await conn.manager.update(Role, () => "", {
+                access: access as AccessKey,
+                prevent_edit: false,
+            });
+        });
+
+        afterEach(async () => {
+            await conn.manager.update(Role, () => "", {
+                access: "ADMIN",
+                prevent_edit: true,
+            });
+            await conn.manager.insert(Department, {
+                name,
+                business_id,
+                updated_by_user_id: user_id,
+            });
+        });
+
+        test(access === "ADMIN" ? "success" : "fail", async () => {
+            await deleteController(
+                {
+                    session: {
+                        user_id,
+                        current_business_id: business_id,
+                        business_ids: [business_id],
+                    },
+                    params: { id },
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+
+            if (access === "ADMIN") {
+                expect(res.sendStatus).toHaveBeenCalledWith(200);
+                expect(
+                    (
+                        await conn.manager.find(Department, {
+                            where: { id, business_id },
+                        })
+                    ).length
+                ).toBe(0);
+            } else {
+                expect(res.sendStatus).toHaveBeenCalledWith(403);
+            }
+        });
+    });
 });
--- src/routes/departments/{id}/delete.ts
+++ src/routes/departments/{id}/delete.ts
@@ -1,10 +1,55 @@
+import Department from "@models/department";
+import User from "@models/user/user";
+import isNumber from "@noahvarghese/get_j_opts/build/lib/isNumber";
 import Logs from "@noahvarghese/logger";
 import { Request, Response } from "express";
 
 const deleteController = async (req: Request, res: Response): Promise<void> => {
-    Logs.Debug(req);
+    const {
+        session: { current_business_id, user_id },
+        params: { id },
+        dbConnection,
+    } = req;
+
+    if (!isNumber(id)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const isAdmin = await User.isAdmin(
+            dbConnection,
+            current_business_id ?? NaN,
+            user_id ?? NaN
+        );
+
+        if (!isAdmin) {
+            res.sendStatus(403);
+            return;
+        }
+    } catch (_e) {
+        const { message } = _e as Error;
+        Logs.Error(message);
+        res.sendStatus(500);
+        return;
+    }
+
+    const dept = await dbConnection.manager.findOne(Department, id);
+
+    if (!dept) {
+        res.sendStatus(400);
+        return;
+    } else if (dept.prevent_delete) {
+        res.sendStatus(405);
+        return;
+    }
+
+    await dbConnection.manager.delete(Department, {
+        business_id: current_business_id,
+        id,
+    });
+
     res.sendStatus(200);
-    return;
 };
 
 export default deleteController;
