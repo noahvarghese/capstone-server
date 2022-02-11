f05de79a28c3c9e0520279118669e87c9f82a0c9
noahvarghese <varghese.noah@gmail.com>
2022-02-11 17:23:17 +0000

    increased test coverage

--- src/routes/departments/{id}/get.ts
+++ src/routes/departments/{id}/get.ts
@@ -1,9 +1,85 @@
+import Department from "@models/department";
+import Role from "@models/role";
+import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
+import isNumber from "@noahvarghese/get_j_opts/build/lib/isNumber";
 import Logs from "@noahvarghese/logger";
 import { Request, Response } from "express";
 
 const getController = async (req: Request, res: Response): Promise<void> => {
-    Logs.Debug(req);
-    res.sendStatus(200);
+    const {
+        session: { user_id, current_business_id },
+        dbConnection,
+        params: { id },
+    } = req;
+
+    if (!isNumber(id)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const [isAdmin, isManager] = await Promise.all([
+            User.isAdmin(
+                dbConnection,
+                current_business_id ?? NaN,
+                user_id ?? NaN
+            ),
+            User.isManager(
+                dbConnection,
+                current_business_id ?? NaN,
+                user_id ?? NaN
+            ),
+        ]);
+
+        if (!(isAdmin || isManager)) {
+            res.sendStatus(403);
+            return;
+        }
+    } catch (_e) {
+        const { message } = _e as Error;
+        Logs.Error(message);
+        res.sendStatus(500);
+        return;
+    }
+
+    try {
+        await dbConnection.manager.findOneOrFail(Department, {
+            where: { business_id: current_business_id, id },
+        });
+    } catch (_e) {
+        const { message } = _e as Error;
+        Logs.Error(message);
+        res.sendStatus(400);
+        return;
+    }
+
+    const result = await dbConnection
+        .createQueryBuilder()
+        .select("d.name", "name")
+        .addSelect("d.id", "id")
+        .addSelect(
+            "(SELECT COUNT(DISTINCT(ur.user_id)) FROM user_role ur JOIN role r ON r.id = ur.role_id JOIN department d2 ON d2.id = r.department_id WHERE r.access = 'MANAGER' AND d2.id = d.id)",
+            "num_managers"
+        )
+        .addSelect(
+            "(SELECT COUNT(DISTINCT(ur.user_id)) FROM user_role ur JOIN role r ON r.id = ur.role_id JOIN department d2 ON d2.id = r.department_id WHERE d2.id = d.id)",
+            "num_members"
+        )
+        .addSelect(
+            "(SELECT COUNT(DISTINCT(r.id)) FROM role r JOIN department d2 ON d2.id = r.department_id WHERE d2.id = d.id)",
+            "num_roles"
+        )
+        .from(Department, "d")
+        .leftJoin(Role, "r", "r.department_id = d.id")
+        .leftJoin(UserRole, "ur", "ur.role_id = r.id")
+        .where("d.business_id = :current_business_id", {
+            current_business_id,
+        })
+        .andWhere("d.id = :id", { id })
+        .getRawOne();
+
+    res.status(200).send(result);
     return;
 };
 
