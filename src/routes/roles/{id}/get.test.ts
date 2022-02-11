8b19aa144004459fca43631bb5c164b7bc621108
noahvarghese <varghese.noah@gmail.com>
2022-02-11 21:45:02 +0000

    added get individual route

---
+++ src/routes/roles/{id}/get.test.ts
@@ -0,0 +1,123 @@
+import Role, { AccessKey } from "@models/role";
+import DBConnection from "@test/support/db_connection";
+import getController from "./get";
+import { Request } from "express";
+import { getMockRes } from "@jest-mock/express";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { Connection } from "typeorm";
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
+let business_id: number, user_id: number;
+let conn: Connection;
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+    ({ business_id, user_id } = await setupAdmin(conn));
+});
+
+afterAll(async () => {
+    const conn = await DBConnection.get();
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+test("bad connection", async () => {
+    await getController(
+        {
+            session: {
+                user_id,
+                business_ids: [business_id],
+                current_business_id: business_id,
+            },
+            params: {
+                id: 1,
+            },
+        } as unknown as Request,
+        res
+    );
+    expect(res.sendStatus).toHaveBeenLastCalledWith(500);
+});
+
+describe("invalid session", () => {
+    const cases = [
+        { user_id: undefined, current_business_id: 1, business_ids: [1] },
+        { user_id: 1, current_business_id: undefined, business_ids: [1] },
+    ];
+    test.each(cases)("%p", async (session) => {
+        await getController(
+            {
+                session,
+                params: {
+                    id: 1,
+                },
+            } as unknown as Request,
+            res
+        );
+        expect(res.sendStatus).toHaveBeenLastCalledWith(500);
+    });
+});
+describe("invalid id", () => {
+    const cases = [{ id: "YOLO" }, { id: 10000 }];
+    test.each(cases)("%p", async ({ id }) => {
+        await getController(
+            {
+                session: {
+                    user_id,
+                    business_ids: [business_id],
+                    current_business_id: business_id,
+                },
+                params: {
+                    id,
+                },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+        expect(res.sendStatus).toHaveBeenLastCalledWith(400);
+    });
+});
+
+describe("permissions", () => {
+    const cases = [
+        { access: "ADMIN", success: true },
+        { access: "MANAGER", success: true },
+        { access: "USER", success: false },
+    ];
+    test.each(cases)("%p", async ({ access }) => {
+        await conn.manager.update(Role, () => "", {
+            access: access as AccessKey,
+            prevent_edit: false,
+        });
+        const role = await conn.manager.findOneOrFail(Role);
+        await getController(
+            {
+                session: {
+                    user_id,
+                    business_ids: [business_id],
+                    current_business_id: business_id,
+                },
+                params: {
+                    id: role.id,
+                },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+
+        if (access === "USER") {
+            expect(res.sendStatus).toHaveBeenLastCalledWith(403);
+        } else {
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.arrayContaining([
+                    expect.objectContaining({ name: role.name }),
+                ])
+            );
+        }
+    });
+});
--- src/routes/roles/{id}/index.ts
+++ src/routes/roles/{id}/index.ts
@@ -1,12 +1,8 @@
-import authenticated from "@middleware/authenticated";
 import { Router } from "express";
-import idRouter from "./{id}";
 import getController from "./get";
 
 const router = Router();
 
-router.use(authenticated);
-router.use("/:id", idRouter);
 router.get("/", getController);
 
 export default router;
