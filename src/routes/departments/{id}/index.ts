90d770d820880f7d2ede5e20df19003b0c825534
noahvarghese <varghese.noah@gmail.com>
2022-02-11 18:31:33 +0000

    added required id

--- src/routes/departments/{id}/index.ts
+++ src/routes/departments/{id}/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import requireParam from "@middleware/require_param";
 import deleteController from "./delete";
 import getController from "./get";
 import putController from "./put";
 
 const router = Router();
+
+const middleware = requireParam("id", (val: string) => !isNaN(Number(val)));
+router.use(middleware);
 
 router.get("/", getController);
 router.put("/", putController);
