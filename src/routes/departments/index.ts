113de62eef90223c033195ff1506bfc55899bd0c
noahvarghese <varghese.noah@gmail.com>
2022-02-11 17:25:58 +0000

    added placeholders for test and linked controller

--- src/routes/departments/index.ts
+++ src/routes/departments/index.ts
@@ -1,12 +1,14 @@
 import authenticated from "@middleware/authenticated";
 import { Router } from "express";
 import getController from "./get";
+import postController from "./post";
 import idRouter from "./{id}";
 
 const router = Router();
 
 router.use(authenticated);
 router.get("/", getController);
+router.post("/", postController);
 router.use("/:id", idRouter);
 
 export default router;
---
+++ src/routes/departments/post.test.ts
@@ -0,0 +1,8 @@
+describe("permissions", () => {
+    const cases = [
+        { access: "ADMIN" },
+        { access: "MANAGER" },
+        { access: "USER" },
+    ];
+    test.todo(cases.toString());
+});
