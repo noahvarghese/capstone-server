2766b0995da7bcf2c513db091ddf9fb7616b50a6
noahvarghese <varghese.noah@gmail.com>
2022-02-11 17:44:42 +0000

    added post tests

--- src/routes/departments/post.test.ts
+++ src/routes/departments/post.test.ts
@@ -1,8 +1,130 @@
+import { getMockRes } from "@jest-mock/express";
+import Department from "@models/department";
+import Role, { AccessKey } from "@models/role";
+import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { Request } from "express";
+import { Connection } from "typeorm";
+import postController from "./post";
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
+let business_id: number, user_id: number;
+let conn: Connection;
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+    ({ business_id, user_id } = await setupAdmin(await DBConnection.get()));
+});
+
+afterAll(async () => {
+    const conn = await DBConnection.get();
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+test("bad db connection", async () => {
+    await postController(
+        {
+            session: { user_id, current_business_id: business_id },
+            body: { name: "TEst" },
+        } as unknown as Request,
+        res
+    );
+    expect(res.sendStatus).toHaveBeenCalledWith(500);
+});
+
+describe("missing session", () => {
+    const cases = [
+        { user_id: undefined, current_business_id: 1 },
+        { user_id: 1, current_business_id: undefined },
+    ];
+
+    test.each(cases)("%p", async (session) => {
+        await postController({ session } as unknown as Request, res);
+        expect(res.sendStatus).toHaveBeenCalledWith(401);
+    });
+});
+
+describe("invalid name", () => {
+    const cases = [
+        { name: 123 },
+        { name: "" },
+        { name: " " },
+        { name: NaN },
+        { name: undefined },
+        { name: null },
+    ];
+
+    test.each(cases)("%p", async (body) => {
+        await postController(
+            {
+                session: {
+                    user_id,
+                    current_business_id: business_id,
+                    business_ids: [business_id],
+                },
+                body,
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+});
 describe("permissions", () => {
     const cases = [
         { access: "ADMIN" },
         { access: "MANAGER" },
         { access: "USER" },
     ];
-    test.todo(cases.toString());
+
+    const name = "TEST";
+
+    describe.each(cases)("%p", ({ access }) => {
+        beforeEach(async () => {
+            await conn.manager.update(Role, () => "", {
+                access: access as AccessKey,
+                prevent_edit: false,
+            });
+        });
+
+        afterEach(async () => {
+            await conn.manager.update(Role, () => "", {
+                access: "ADMIN",
+                prevent_edit: true,
+            });
+            await conn.manager.delete(Department, { name });
+        });
+
+        test(access === "ADMIN" ? "success" : "fail", async () => {
+            await postController(
+                {
+                    session: {
+                        user_id,
+                        current_business_id: business_id,
+                        business_ids: [business_id],
+                    },
+                    body: { name },
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+
+            if (access === "ADMIN") {
+                expect(res.sendStatus).toHaveBeenCalledWith(201);
+                expect(
+                    await conn.manager.findOneOrFail(Department, {
+                        where: { name, business_id },
+                    })
+                ).toBeTruthy();
+            } else {
+                expect(res.sendStatus).toHaveBeenCalledWith(403);
+            }
+        });
+    });
 });
--- src/routes/departments/post.ts
+++ src/routes/departments/post.ts
@@ -1,10 +1,61 @@
+import Department from "@models/department";
+import User from "@models/user/user";
+import getJOpts from "@noahvarghese/get_j_opts";
+import isNumber from "@noahvarghese/get_j_opts/build/lib/isNumber";
 import Logs from "@noahvarghese/logger";
 import { Request, Response } from "express";
 
 const postController = async (req: Request, res: Response): Promise<void> => {
-    Logs.Debug(req);
-    res.sendStatus(200);
-    return;
+    const {
+        session: { current_business_id, user_id },
+        dbConnection,
+    } = req;
+
+    // despite checking in the middleware, we need to explicitly define these as numbers
+    if (!isNumber(user_id) || !isNumber(current_business_id)) {
+        res.sendStatus(401);
+        return;
+    }
+    let name = "";
+
+    try {
+        const data = getJOpts(req.body, {
+            name: { type: "string", required: true },
+        });
+        name = data.name as string;
+    } catch (_e) {
+        const { message } = _e as Error;
+        res.status(400).send(message);
+        return;
+    }
+
+    try {
+        const isAdmin = await User.isAdmin(
+            dbConnection,
+            current_business_id ?? NaN,
+            user_id ?? NaN
+        );
+
+        if (!isAdmin) {
+            res.sendStatus(403);
+            return;
+        }
+    } catch (_e) {
+        const { message } = _e as Error;
+        Logs.Error(message);
+        res.sendStatus(500);
+        return;
+    }
+
+    await dbConnection.manager.insert(
+        Department,
+        new Department({
+            name,
+            updated_by_user_id: user_id,
+            business_id: current_business_id,
+        })
+    );
+    res.sendStatus(201);
 };
 
 export default postController;
