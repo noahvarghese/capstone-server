9349513955a0aee7435914291db4bff39fdc0658
noahvarghese <varghese.noah@gmail.com>
2022-02-11 17:04:23 +0000

    Added files for next controller/route

---
+++ src/routes/departments/{id}/get.test.ts
@@ -0,0 +1,4 @@
+describe("permissions", () => {
+    test.todo("");
+    return;
+});
--- src/routes/departments/{id}/index.ts
+++ src/routes/departments/{id}/index.ts
@@ -1,12 +1,8 @@
-import authenticated from "@middleware/authenticated";
 import { Router } from "express";
 import getController from "./get";
-import idRouter from "./{id}";
 
 const router = Router();
 
-router.use(authenticated);
 router.get("/", getController);
-router.use("/:id", idRouter);
 
 export default router;
