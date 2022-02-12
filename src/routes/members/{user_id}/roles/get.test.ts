77f6565c3e5403a32fefbfab475ffcd0437e6587
noahvarghese <varghese.noah@gmail.com>
2022-02-12 00:03:35 +0000

    added route to get all roles a member is associated with

--- src/routes/members/{user_id}/roles/get.test.ts
+++ src/routes/members/{user_id}/roles/get.test.ts
@@ -1,3 +1,183 @@
-describe("", () => {
-    test.todo("")
-})
+import { getMockRes } from "@jest-mock/express";
+import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { Connection } from "typeorm";
+import getController from "./get";
+import { Request } from "express";
+import Role, { AccessKey } from "@models/role";
+import Department from "@models/department";
+import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
+let business_id: number, user_id: number;
+let conn: Connection;
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+    ({ business_id, user_id } = await setupAdmin(conn));
+});
+
+afterAll(async () => {
+    const conn = await DBConnection.get();
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+test("bad connection", async () => {
+    await getController(
+        {
+            session: {
+                user_id,
+                business_ids: [business_id],
+                current_business_id: business_id,
+            },
+            params: {
+                user_id: 1,
+            },
+        } as unknown as Request,
+        res
+    );
+    expect(res.sendStatus).toHaveBeenLastCalledWith(500);
+});
+
+describe("invalid session", () => {
+    const cases = [
+        { user_id: undefined, current_business_id: 1, business_ids: [1] },
+        { user_id: 1, current_business_id: undefined, business_ids: [1] },
+    ];
+    test.each(cases)("%p", async (session) => {
+        await getController(
+            {
+                session,
+                params: {
+                    user_id: 1,
+                },
+            } as unknown as Request,
+            res
+        );
+        expect(res.sendStatus).toHaveBeenLastCalledWith(401);
+    });
+});
+
+describe("invalid id", () => {
+    const cases = [{ id: "YOLO" }, { id: 10000 }];
+    test.each(cases)("%p", async ({ id }) => {
+        await getController(
+            {
+                session: {
+                    user_id,
+                    business_ids: [business_id],
+                    current_business_id: business_id,
+                },
+                params: {
+                    user_id: id,
+                },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+        expect(res.sendStatus).toHaveBeenLastCalledWith(400);
+    });
+});
+
+describe("requires second user with no permissions", () => {
+    let newUserId: number, role_id: number;
+
+    beforeAll(async () => {
+        const { id } = await conn.manager.findOneOrFail(Department);
+        ({
+            identifiers: [{ id: role_id }],
+        } = await conn.manager.insert(
+            Role,
+            new Role({
+                department_id: id,
+                updated_by_user_id: user_id,
+                access: "USER",
+                name: "TEST",
+            })
+        ));
+
+        ({
+            identifiers: [{ id: newUserId }],
+        } = await conn.manager.insert(
+            User,
+            new User({
+                first_name: "TESt",
+                last_name: "TEST",
+                email: "TEST",
+                password: "TEST",
+            })
+        ));
+        await conn.manager.insert(
+            UserRole,
+            new UserRole({
+                user_id: newUserId,
+                role_id,
+                updated_by_user_id: user_id,
+            })
+        );
+    });
+
+    afterAll(async () => {
+        await conn.manager.delete(UserRole, { user_id: newUserId });
+        await conn.manager.delete(User, newUserId);
+        await conn.manager.delete(Role, role_id);
+    });
+    test("self access with underprivleled user", async () => {
+        await getController(
+            {
+                session: {
+                    user_id: newUserId,
+                    business_ids: [business_id],
+                    current_business_id: business_id,
+                },
+                params: {
+                    user_id: newUserId,
+                },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+        expect(res.status).toHaveBeenLastCalledWith(200);
+    });
+
+    describe("permissions", () => {
+        const cases = [
+            { access: "ADMIN", success: true },
+            { access: "MANAGER", success: true },
+            { access: "USER", success: false },
+        ];
+        test.each(cases)("%p", async ({ access }) => {
+            await conn.manager.update(Role, () => "", {
+                access: access as AccessKey,
+                prevent_edit: false,
+            });
+            await getController(
+                {
+                    session: {
+                        user_id,
+                        business_ids: [business_id],
+                        current_business_id: business_id,
+                    },
+                    params: {
+                        user_id: newUserId,
+                    },
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+
+            if (access === "USER") {
+                expect(res.sendStatus).toHaveBeenLastCalledWith(403);
+            } else {
+                expect(res.status).toHaveBeenCalledWith(200);
+            }
+        });
+    });
+});
--- src/routes/members/{user_id}/roles/get.ts
+++ src/routes/members/{user_id}/roles/get.ts
@@ -1,10 +1,108 @@
+import Department from "@models/department";
+import Role from "@models/role";
+import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
+import isNumber from "@noahvarghese/get_j_opts/build/lib/isNumber";
 import Logs from "@noahvarghese/logger";
 import { Request, Response } from "express";
 
 const getController = async (req: Request, res: Response): Promise<void> => {
-    Logs.Debug(req);
-    res.sendStatus(200);
-    return;
-}
+    const {
+        params: { user_id: userId },
+        session: { user_id, current_business_id },
+        dbConnection,
+    } = req;
+
+    const id = Number(userId);
+
+    if (!isNumber(id)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    // despite checking in the middleware, we need to explicitly define these as numbers
+    if (!isNumber(user_id) || !isNumber(current_business_id)) {
+        res.sendStatus(401);
+        return;
+    }
+
+    if (id !== user_id) {
+        try {
+            const [isAdmin, isManager] = await Promise.all([
+                User.isAdmin(
+                    dbConnection,
+                    current_business_id ?? NaN,
+                    user_id ?? NaN
+                ),
+                User.isManager(
+                    dbConnection,
+                    current_business_id ?? NaN,
+                    user_id ?? NaN
+                ),
+            ]);
+
+            if (!(isAdmin || isManager)) {
+                res.sendStatus(403);
+                return;
+            }
+        } catch (_e) {
+            const { message } = _e as Error;
+            Logs.Error(message);
+            res.sendStatus(500);
+            return;
+        }
+    }
+
+    if (!dbConnection || !dbConnection.isConnected) {
+        res.sendStatus(500);
+        return;
+    }
+
+    try {
+        await dbConnection.manager.findOneOrFail(User, id);
+    } catch (_e) {
+        const { message } = _e as Error;
+        Logs.Error(message);
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const result = await dbConnection
+            .createQueryBuilder()
+            .select("r.id", "id")
+            .addSelect("r.name", "name")
+            .addSelect("d.name", "department_name")
+            .addSelect("d.id", "department_id")
+            .from(Role, "r")
+            .leftJoin(UserRole, "ur", "ur.role_id = r.id")
+            .leftJoin(Department, "d", "d.id = r.department_id")
+            .where("d.business_id = :current_business_id", {
+                current_business_id,
+            })
+            .andWhere("ur.user_id = :id", { id })
+            .getRawMany<{
+                id: number;
+                name: string;
+                department_id: number;
+                department_name: string;
+            }>();
+
+        res.status(200).send(
+            result.map(({ id, name, ...rest }) => ({
+                id,
+                name,
+                department: {
+                    id: rest.department_id,
+                    name: rest.department_name,
+                },
+            }))
+        );
+    } catch (_e) {
+        const { message } = _e as Error;
+        Logs.Error(message);
+        res.sendStatus(500);
+    }
+};
 
 export default getController;
