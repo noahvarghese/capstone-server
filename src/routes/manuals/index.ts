3141db81ff3f2b011066d588878092012c5aadd6
noahvarghese <varghese.noah@gmail.com>
2022-02-12 19:48:18 +0000

    added get controller with test file and attached to route

--- src/routes/manuals/index.ts
+++ src/routes/manuals/index.ts
@@ -1,8 +1,11 @@
 import authenticated from "@middleware/authenticated";
 import { Router } from "express";
+import getController from "./get";
 
 const router = Router();
 
 router.use(authenticated);
+
+router.get("/", getController);
 
 export default router;
