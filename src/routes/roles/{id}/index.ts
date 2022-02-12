e910e731811389487e88ba6660ed8b9596b9ca39
noahvarghese <varghese.noah@gmail.com>
2022-02-12 18:44:13 +0000

    added in new route with placeholder test and controller

--- src/routes/roles/{id}/index.ts
+++ src/routes/roles/{id}/index.ts
@@ -2,8 +2,11 @@ import deleteController from "@routes/members/{user_id}/delete";
 import { Router } from "express";
 import getController from "./get";
 import putController from "./put";
+import membersRoute from "./members";
 
 const router = Router();
+
+router.use("/members", membersRoute);
 
 router.get("/", getController);
 router.put("/", putController);
