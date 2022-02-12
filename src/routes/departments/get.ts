ce73fddcdb0fe534c16d3b3af40c33f8be81abbe
noahvarghese <varghese.noah@gmail.com>
2022-02-12 16:44:43 +0000

    removed check of session

--- src/routes/departments/get.ts
+++ src/routes/departments/get.ts
@@ -60,12 +60,10 @@ const getController = async (req: Request, res: Response): Promise<void> => {
     }
 
     const [isAdmin, isManager] = await Promise.all([
-        User.isAdmin(dbConnection, current_business_id ?? NaN, user_id ?? NaN),
-        User.isManager(
-            dbConnection,
-            current_business_id ?? NaN,
-            user_id ?? NaN
-        ),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isAdmin(dbConnection, current_business_id!, user_id!),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isManager(dbConnection, current_business_id!, user_id!),
     ]);
 
     if (!(isAdmin || isManager)) {
