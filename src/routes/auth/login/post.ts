031f65e14032113eb44d1d817039250ea39fd92a
noahvarghese <varghese.noah@gmail.com>
2022-02-12 16:29:14 +0000

    added check for database connection so I can remove extra try catches

--- src/routes/auth/login/post.ts
+++ src/routes/auth/login/post.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import Membership from "@models/membership";
 import User from "@models/user/user";
-import Logs from "@noahvarghese/logger";
 import { bodyValidators } from "@util/formats/body";
 import getJOpts, { Expected } from "@noahvarghese/get_j_opts";
 
@@ -42,47 +41,43 @@ export const loginController = async (
         return;
     }
 
-    try {
-        const user = await connection.manager.findOne(User, {
-            where: { email },
-        });
-
-        // Not logged in
-        if (
-            !user ||
-            !user.password ||
-            !(await user.comparePassword(password))
-        ) {
-            res.sendStatus(401);
-            return;
-        }
-
-        const m = await connection.manager.find(Membership, {
-            where: { user_id: user.id },
-        });
-
-        if (m.length === 0) {
-            res.status(403).send("Please contact your manager");
-            return;
-        }
-
-        const defaultBusinessId =
-            m.find((x) => x.default_option && x.accepted)?.business_id ?? NaN;
-
-        if (isNaN(defaultBusinessId)) {
-            res.status(500).send("No default business set");
-            return;
-        }
-
-        req.session.business_ids = m.map((x) => x.business_id);
-        req.session.current_business_id = defaultBusinessId;
-        req.session.user_id = user.id;
-        res.status(200).send(user.id);
-    } catch (_e) {
-        const { message } = _e as Error;
-        Logs.Error(message);
+    if (!connection || !connection.isConnected) {
         res.sendStatus(500);
+        return;
     }
+
+    const user = await connection.manager.findOne(User, {
+        where: { email },
+    });
+
+    // Not logged in
+    if (!user || !user.password || !(await user.comparePassword(password))) {
+        res.sendStatus(401);
+        return;
+    }
+
+    const m = await connection.manager.find(Membership, {
+        where: { user_id: user.id },
+    });
+
+    if (m.length === 0) {
+        res.status(403).send("Please contact your manager");
+        return;
+    }
+
+    const defaultBusinessId =
+        m.find((x) => x.default_option && x.accepted)?.business_id ?? NaN;
+
+    if (isNaN(defaultBusinessId)) {
+        res.status(500).send("No default business set");
+        return;
+    }
+
+    req.session.business_ids = m.map((x) => x.business_id);
+    req.session.current_business_id = defaultBusinessId;
+    req.session.user_id = user.id;
+
+    res.status(200).send(user.id);
 
     return;
 };
