345a3ae473598fe4cd4e862c6e80a0c6a756e0da
noahvarghese <varghese.noah@gmail.com>
2022-02-14 17:37:53 +0000

    added create manual route

--- src/routes/manuals/index.ts
+++ src/routes/manuals/index.ts
@@ -1,11 +1,13 @@
 import authenticated from "@middleware/authenticated";
 import { Router } from "express";
 import getController from "./get";
+import postController from "./post";
 
 const router = Router();
 
 router.use(authenticated);
 
 router.get("/", getController);
+router.post("/", postController);
 
 export default router;
---
+++ src/routes/manuals/post.test.ts
@@ -0,0 +1,78 @@
+import { getMockRes } from "@jest-mock/express";
+import Manual from "@models/manual/manual";
+import Role, { AccessKey } from "@models/role";
+import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { Request } from "express";
+import { SessionData } from "express-session";
+import { Connection } from "typeorm";
+import postController from "./post";
+
+let business_id: number, user_id: number;
+let conn: Connection;
+let session: Omit<SessionData, "cookie">;
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+
+    ({ business_id, user_id } = await setupAdmin(conn));
+
+    session = {
+        user_id,
+        business_ids: [business_id],
+        current_business_id: business_id,
+    };
+});
+
+afterAll(async () => {
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+describe("Permissions", () => {
+    const cases = ["ADMIN", "MANAGER", "USER"];
+
+    afterEach(async () => {
+        await conn.manager.delete(Manual, () => "");
+        await conn.manager.update(Role, () => "", {
+            access: "ADMIN",
+            prevent_edit: true,
+        });
+    });
+
+    test.each(cases)("%p", async (access) => {
+        await conn.manager.update(Role, () => "", {
+            access: access as AccessKey,
+            prevent_edit: false,
+        });
+
+        await postController(
+            { session, dbConnection: conn, body: { title: "TEST" } } as Request,
+            res
+        );
+
+        if (access !== "USER") {
+            expect(res.sendStatus).toHaveBeenCalledWith(201);
+            const mans = await conn.manager.find(Manual);
+            expect(mans.length).toBe(1);
+            expect(mans[0].published).toBe(false);
+        } else {
+            expect(res.sendStatus).toHaveBeenCalledWith(403);
+        }
+    });
+});
+
+test("invalid title", async () => {
+    await postController(
+        { session, dbConnection: conn, body: {} } as Request,
+        res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(400);
+});
