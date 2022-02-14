eafb8f5ba2be4b09853ea58b4a0e2131d495065f
noahvarghese <varghese.noah@gmail.com>
2022-02-14 15:19:24 +0000

    got manual read working

--- src/routes/manuals/get.test.ts
+++ src/routes/manuals/get.test.ts
@@ -1,3 +1,288 @@
-describe("", () => {
+import { getMockRes } from "@jest-mock/express";
+import Role, { AccessKey } from "@models/role";
+import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { SessionData } from "express-session";
+import { Connection } from "typeorm";
+import getController from "./get";
+import { Request, Response } from "express";
+import { deepClone } from "@util/obj";
+import Manual from "@models/manual/manual";
+import ManualAssignment from "@models/manual/assignment";
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
+let business_id: number, user_id: number, role_id: number;
+const manual_ids: number[] = [];
+let conn: Connection;
+let session: Omit<SessionData, "cookie">;
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+
+    ({ business_id, user_id } = await setupAdmin(conn));
+
+    session = {
+        user_id,
+        business_ids: [business_id],
+        current_business_id: business_id,
+    };
+
+    let {
+        identifiers: [{ id }],
+    } = await conn.manager.insert(
+        Manual,
+        new Manual({
+            title: "TEST",
+            prevent_delete: false,
+            prevent_edit: false,
+            published: true,
+            updated_by_user_id: user_id,
+        })
+    );
+
+    manual_ids.push(id);
+    ({
+        identifiers: [{ id }],
+    } = await conn.manager.insert(
+        Manual,
+        new Manual({
+            title: "TEST123",
+            prevent_delete: false,
+            prevent_edit: false,
+            published: true,
+            updated_by_user_id: user_id,
+        })
+    ));
+
+    manual_ids.push(id);
+
+    role_id = (await conn.manager.findOneOrFail(Role)).id;
+
+    for (const manual_id of manual_ids) {
+        await conn.manager.insert(
+            ManualAssignment,
+            new ManualAssignment({
+                manual_id,
+                updated_by_user_id: user_id,
+                role_id,
+            })
+        );
+    }
+});
+
+afterAll(async () => {
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+describe("sort", () => {
+    const cases = [
+        { sort_field: "title", sort_order: "ASC" },
+        { sort_field: "title", sort_order: "DESC" },
+    ];
+
+    test.each(cases)(
+        "given sort field %p and sort order %p, the results will match",
+        async ({ sort_field, sort_order }) => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            let data: { [x: string]: any }[] = [];
+
+            await getController(
+                {
+                    session,
+                    query: { sort_field, sort_order },
+                    dbConnection: conn,
+                } as unknown as Request,
+                {
+                    status: () => {
+                        return {
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            send: (u: { [x: string]: any }[]) => {
+                                data = u;
+                            },
+                        };
+                    },
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    send: (u: { [x: string]: any }[]) => {
+                        data = u;
+                    },
+                } as unknown as Response
+            );
+            expect(data.length).toBeGreaterThan(1);
+            const dataCopy = deepClone(data);
+            expect(dataCopy).toBeInstanceOf(Array);
+            const sortedData = dataCopy.sort((a, b): number => {
+                const aVal = JSON.stringify(a[sort_field]);
+                const bVal = JSON.stringify(b[sort_field]);
+
+                if (sort_order === "ASC") {
+                    return aVal < bVal ? -1 : aVal === bVal ? 0 : 1;
+                } else {
+                    return aVal < bVal ? 1 : aVal === bVal ? 0 : -1;
+                }
+            });
+
+            expect(JSON.stringify(data)).toBe(JSON.stringify(sortedData));
+        }
+    );
+});
+
+describe("search", () => {
+    const cases = [
+        {
+            search: "123",
+        },
+    ];
+
+    test.each(cases)("%p", async ({ search }) => {
+        let status;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let send: any;
+        await getController(
+            {
+                query: { search },
+                session,
+                dbConnection: conn,
+            } as unknown as Request,
+            {
+                send: (v: unknown) => {
+                    send = v;
+                },
+                status: (v: unknown) => {
+                    status = v;
+                    return {
+                        send: (v: unknown) => {
+                            send = v;
+                        },
+                    };
+                },
+            } as Response
+        );
+        expect(status).toBe(200);
+        expect(send.length).toBe(1);
+        expect((send[0].title as string).toLowerCase()).toContain(
+            search.toLowerCase()
+        );
+    });
+});
+
+describe("pagination", () => {
+    describe("page", () => {
+        const cases = [
+            { limit: 1, page: 1 },
+            { limit: 1, page: 2 },
+        ];
+        test.each(cases)("%p", async ({ limit, page }) => {
+            const conn = await DBConnection.get();
+            await getController(
+                {
+                    query: { limit, page },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({ length: 1 })
+            );
+        });
+    });
+
+    describe("limit", () => {
+        const cases = [
+            { limit: 1, page: 1 },
+            { limit: 2, page: 1 },
+        ];
+        test.each(cases)("%p", async ({ limit, page }) => {
+            const conn = await DBConnection.get();
+            let status;
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            let send: any;
+            await getController(
+                {
+                    query: { limit, page },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                {
+                    send: (v: unknown) => {
+                        send = v;
+                    },
+                    status: (v: unknown) => {
+                        status = v;
+                        return {
+                            send: (v: unknown) => {
+                                send = v;
+                            },
+                        };
+                    },
+                } as Response
+            );
+            expect(status).toBe(200);
+            expect(send.length).toBe(limit);
+        });
+    });
+});
+
+describe("filter", () => {
     test.todo("");
+});
+
+describe("published", () => {
+    const published = [true, false];
+    describe.each(published)("%p", (p) => {
+        const permissions = ["ADMIN", "MANAGER", "USER"];
+        beforeAll(async () => {
+            Promise.all(
+                manual_ids.map((m) =>
+                    conn.manager.update(Manual, m, {
+                        published: p,
+                    })
+                )
+            );
+        });
+        describe.each(permissions)("permissions %p", (access) => {
+            beforeAll(async () => {
+                await conn.manager.update(Role, role_id, {
+                    access: access as AccessKey,
+                    prevent_edit: false,
+                });
+            });
+            afterAll(async () => {
+                await conn.manager.update(Role, role_id, {
+                    access: "ADMIN",
+                    prevent_edit: true,
+                });
+            });
+
+            test("", async () => {
+                await getController(
+                    {
+                        session,
+                        query: {},
+                        dbConnection: conn,
+                    } as unknown as Request,
+                    res
+                );
+
+                expect(res.status).toHaveBeenCalledWith(200);
+
+                if (access === "USER" && p === false) {
+                    expect(res.send).toHaveBeenCalledWith(
+                        expect.objectContaining({ length: 0 })
+                    );
+                } else {
+                    expect(res.send).toHaveBeenCalledWith(
+                        expect.objectContaining({ length: 2 })
+                    );
+                }
+            });
+        });
+    });
 });
--- src/routes/manuals/get.ts
+++ src/routes/manuals/get.ts
@@ -1,14 +1,18 @@
-import { AccessKey } from "@models/role";
+import Department from "@models/department";
+import ManualAssignment from "@models/manual/assignment";
+import Manual from "@models/manual/manual";
+import Role from "@models/role";
 import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
 import isNumber from "@noahvarghese/get_j_opts/build/lib/isNumber";
 import { isJson } from "@util/obj";
 import { Request, Response } from "express";
+import { Connection, SelectQueryBuilder } from "typeorm";
 
 const filterFields = ["department", "role"] as const;
-
 type FilterKey = typeof filterFields[number];
 
-const sortFields = ["name", "role", "department"] as const;
+const sortFields = ["title"] as const;
 type SortFieldKey = typeof sortFields[number];
 
 const sortOrders = ["ASC", "DESC"] as const;
@@ -20,23 +24,126 @@ type ManualResponse = {
     published: boolean;
     prevent_delete: boolean;
     prevent_edit: boolean;
+    editable_by_user: boolean;
 };
 
-type Handler = (query?: {
-    search?: string;
-    filter_ids?: number[];
-}) => ManualResponse[];
+type ManualGetter = (
+    conn: Connection,
+    business_id: number,
+    user_id: number,
+    query?: {
+        search?: string;
+        filter_ids?: number[];
+        filter_field?: FilterKey;
+        sort_field?: SortFieldKey;
+        sort_order?: SortOrderKey;
+        limit?: number;
+        page?: number;
+    }
+) => Promise<ManualResponse[]>;
 
-const permissionHandler: { [x in AccessKey]: Handler } = {
-    ADMIN: () => {
-        return [];
-    },
-    MANAGER: () => {
-        return [];
-    },
-    USER: () => {
-        return [];
-    },
+const executeFilterQuery = async (
+    query: SelectQueryBuilder<ManualResponse>,
+    options?: {
+        search?: string;
+        filter_ids?: number[];
+        filter_field?: FilterKey;
+        sort_field?: SortFieldKey;
+        sort_order?: SortOrderKey;
+        limit?: number;
+        page?: number;
+    }
+): Promise<ManualResponse[]> => {
+    if (!options) {
+        return await query.getRawMany<ManualResponse>();
+    }
+
+    if (options.search) {
+        const sqlizedSearchItem = `%${options.search}%`;
+        query = query.andWhere("m.title like :manual", {
+            manual: sqlizedSearchItem,
+        });
+    }
+
+    query = query.orderBy(
+        options.sort_field ? `m.${options.sort_field}` : "m.created_on",
+        (options.sort_order as SortOrderKey) ?? "DESC"
+    );
+
+    if (options.page && options.limit) {
+        query = query
+            .limit(Number(options.limit))
+            .offset(
+                Number(options.page) * Number(options.limit) -
+                    Number(options.limit)
+            );
+    }
+
+    return await query.getRawMany<ManualResponse>();
+};
+
+const admin: ManualGetter = async (conn, business_id, _, query) => {
+    return await executeFilterQuery(
+        conn
+            .createQueryBuilder()
+            .select(
+                "m.id, m.title, m.published, m.prevent_delete, m.prevent_edit"
+            )
+            .addSelect("1", "editable_by_user")
+            .from(Manual, "m")
+            .leftJoin(ManualAssignment, "ma", "ma.manual_id = m.id")
+            .leftJoin(Role, "r", "r.id = ma.role_id")
+            .leftJoin(Department, "d", "d.id = r.department_id")
+            .where("d.business_id = :business_id", {
+                business_id,
+            }) as unknown as SelectQueryBuilder<ManualResponse>,
+        query
+    );
+};
+
+const manager: ManualGetter = async (conn, business_id, user_id, query) => {
+    return await executeFilterQuery(
+        conn
+            .createQueryBuilder()
+            .select(
+                "m.id, m.title, m.published, m.prevent_delete, m.prevent_edit"
+            )
+            .addSelect("1", "editable_by_user")
+            .from(Manual, "m")
+            .leftJoin(ManualAssignment, "ma", "ma.manual_id = m.id")
+            .leftJoin(Role, "r", "r.id = ma.role_id")
+            .leftJoin(Department, "d", "d.id = r.department_id")
+            .leftJoin(UserRole, "ur", "ur.role_id = r.id")
+            .where("ur.user_id = :user_id", { user_id })
+            .andWhere("r.access = :access", { access: "MANAGER" })
+            .andWhere("d.business_id = :business_id", {
+                business_id,
+            }) as unknown as SelectQueryBuilder<ManualResponse>,
+        query
+    );
+};
+
+const user: ManualGetter = async (conn, business_id, user_id, query) => {
+    return await executeFilterQuery(
+        conn
+            .createQueryBuilder()
+            .select(
+                "m.id, m.title, m.published, m.prevent_delete, m.prevent_edit"
+            )
+            .addSelect("0", "editable_by_user")
+            .from(Manual, "m")
+            .leftJoin(ManualAssignment, "ma", "ma.manual_id = m.id")
+            .leftJoin(Role, "r", "r.id = ma.role_id")
+            .leftJoin(Department, "d", "d.id = r.department_id")
+            .leftJoin(UserRole, "ur", "ur.role_id = r.id")
+            .where("ur.user_id = :user_id", { user_id })
+            .andWhere("r.access = :access", { access: "USER" })
+            .andWhere("m.published = :published", { published: true })
+            .andWhere("d.business_id = :business_id", {
+                business_id,
+            }) as unknown as SelectQueryBuilder<ManualResponse>,
+        query
+    );
 };
 
 const getController = async (req: Request, res: Response): Promise<void> => {
@@ -54,6 +161,7 @@ const getController = async (req: Request, res: Response): Promise<void> => {
         dbConnection,
     } = req;
 
+    // 1.   Check that url query items are correct
     const filter_ids = isJson(filterIds as string)
         ? JSON.parse(filterIds as string)
         : null;
@@ -110,6 +218,7 @@ const getController = async (req: Request, res: Response): Promise<void> => {
         return;
     }
 
+    // 2.   Check permissions
     const [isAdmin, isManager] = await Promise.all([
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         User.isAdmin(dbConnection, current_business_id!, user_id!),
@@ -117,13 +226,41 @@ const getController = async (req: Request, res: Response): Promise<void> => {
         User.isManager(dbConnection, current_business_id!, user_id!),
     ]);
 
-    const access: AccessKey = isAdmin
-        ? "ADMIN"
-        : isManager
-        ? "MANAGER"
-        : "USER";
+    // 3.   Perform body of request
+    const params: Parameters<ManualGetter> = [
+        dbConnection,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        current_business_id!,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        user_id!,
+        {
+            filter_field: filter_field as FilterKey,
+            filter_ids,
+            search: search as string,
+            sort_field: sort_field as SortFieldKey,
+            sort_order: sort_order as SortOrderKey,
+            limit: limit ? Number(limit) : undefined,
+            page: page ? Number(page) : undefined,
+        },
+    ];
 
-    const result = permissionHandler[access]({ search: search as string });
+    let result: ManualResponse[] = [];
+
+    // i.   Get all manuals for business
+    if (isAdmin) {
+        result = await admin(...params);
+    }
+    // ii.   Get all manuals that user manages
+    else if (isManager) {
+        result = await manager(...params);
+    }
+
+    // iii. Since an admin has access to all manuals, and an admin cannot be a user or manager
+    //      but managers may be users in other roles
+    //      so we have to get user manuals as well for users and managers
+    if (!isAdmin) {
+        result = result.concat(await user(...params));
+    }
 
     res.status(200).send(result);
     return;
