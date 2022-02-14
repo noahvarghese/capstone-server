bc02794dc76f2bb2ff063d53d9d14b79d55b0069
noahvarghese <varghese.noah@gmail.com>
2022-02-14 22:04:10 +0000

    added route for manual roles

--- src/routes/manuals/{manual_id}/index.ts
+++ src/routes/manuals/{manual_id}/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import roleRouter from "./roles";
 import deleteController from "./delete";
 import getController from "./get";
 import putController from "./put";
 
 const router = Router();
+
+router.use("/roles", roleRouter);
 
 router.get("/", getController);
 router.put("/", putController);
