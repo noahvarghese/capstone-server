a8efd8fee05da0625934b4c07facc6dd2fa1a10d
noahvarghese <varghese.noah@gmail.com>
2022-02-14 20:56:04 +0000

    Added get manual by id

--- src/routes/manuals/{id}/index.ts
+++ src/routes/manuals/{id}/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import deleteController from "./delete";
+import getController from "./get";
 import putController from "./put";
 
 const router = Router();
 
+router.get("/", getController);
 router.put("/", putController);
 router.delete("/", deleteController);
 
