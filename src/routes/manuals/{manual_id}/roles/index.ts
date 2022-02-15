676a3af8918a3f99ce3be7c905f0abdc0cd709ea
noahvarghese <varghese.noah@gmail.com>
2022-02-15 15:19:25 +0000

    Added role id router

--- src/routes/manuals/{manual_id}/roles/index.ts
+++ src/routes/manuals/{manual_id}/roles/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import roleIdRouter from "./{role_id}";
 import getController from "./get";
 
 const router = Router();
+
+router.use("/:role_id", roleIdRouter);
 
 router.get("/", getController);
 
