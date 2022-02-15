5565e57993d7735518ca1eb51d592156cc0ed6f6
noahvarghese <varghese.noah@gmail.com>
2022-02-15 20:10:05 +0000

    Added get policies

--- src/routes/manuals/sections/{id}/index.ts
+++ src/routes/manuals/sections/{id}/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import policyRouter from "./policies";
 import getController from "./get";
 import putController from "./put";
 
 const router = Router();
+
+router.use("/policies", policyRouter);
 
 router.get("/", getController);
 router.put("/", putController);
---
+++ src/routes/manuals/sections/{id}/policies/get.ts
@@ -0,0 +1,48 @@
+import ManualAssignment from "@models/manual/assignment";
+import Manual from "@models/manual/manual";
+import Policy from "@models/manual/policy";
+import ManualSection from "@models/manual/section";
+import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
+import { Request, Response } from "express";
+
+const getController = async (req: Request, res: Response): Promise<void> => {
+    const {
+        session: { user_id, current_business_id },
+        params: { manual_id },
+        dbConnection,
+    } = req;
+
+    const [isAdmin, isManager] = await Promise.all([
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isAdmin(dbConnection, current_business_id!, user_id!),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isManager(dbConnection, current_business_id!, user_id!),
+    ]);
+
+    let query = dbConnection
+        .createQueryBuilder()
+        .select("p.id, p.title")
+        .from(Policy, "p")
+        .leftJoin(ManualSection, "ms", "ms.id = p.manual_section_id")
+        .leftJoin(Manual, "m", "m.id = ms.manual_id")
+        .leftJoin(ManualAssignment, "ma", "ma.manual_id = m.id")
+        .leftJoin(UserRole, "ur", "ur.role_id = ma.role_id")
+        .where("m.business_id = :current_business_id", {
+            current_business_id,
+        })
+        .andWhere("m.id = :manual_id", { manual_id });
+
+    if (!(isAdmin || isManager)) {
+        query = query
+            .andWhere("m.published = :published", { published: true })
+            .andWhere("ur.user_id = :user_id", { user_id });
+    }
+
+    res.status(200).send(
+        await query.getRawMany<{ id: number; title: string }>()
+    );
+    return;
+};
+
+export default getController;
