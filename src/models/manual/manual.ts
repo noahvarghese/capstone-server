6e9dc6006e095fc8b39d73da889b19342b5ab74b
noahvarghese <varghese.noah@gmail.com>
2022-02-15 15:22:56 +0000

    Added check for the owner of a manual

--- src/models/manual/manual.ts
+++ src/models/manual/manual.ts
@@ -1,6 +1,9 @@
-import { Entity, Column } from "typeorm";
+import Role from "@models/role";
+import UserRole from "@models/user/user_role";
+import { Entity, Column, Connection, Not } from "typeorm";
 import { AttributeFactory } from "../abstract/base_model";
 import EditableContentModel from "../abstract/editable_content_model";
+import ManualAssignment from "./assignment";
 
 export interface ManualAttributes {
     title: string;
@@ -36,4 +39,24 @@ export default class Manual
         super();
         Object.assign(this, AttributeFactory(options, EmptyManualAttributes));
     }
+
+    public static isOwner = async (
+        conn: Connection,
+        user_id: number,
+        manual_id: number
+    ): Promise<boolean> => {
+        return Boolean(
+            await conn
+                .createQueryBuilder()
+                .select("ma")
+                .from(ManualAssignment, "ma")
+                .leftJoin(Role, "r", "ma.role_id = r.id")
+                .leftJoin(UserRole, "ur", "ur.role_id = r.id")
+                .where("ma.manual_id = :manual_id", { manual_id })
+                .andWhere("ur.user_id = :user_id", { user_id })
+                .andWhere("r.access = :access", { access: Not("USER") })
+                .andWhere("ma.owner = :owner", { owner: true })
+                .getOne()
+        );
+    };
 }
