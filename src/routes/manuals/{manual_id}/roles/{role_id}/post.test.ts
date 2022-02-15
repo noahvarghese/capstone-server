e1510a6fecac2aa1d40d439bb3b78be93767f0f4
noahvarghese <varghese.noah@gmail.com>
2022-02-15 16:35:58 +0000

    throw error instead of continuing

--- src/routes/manuals/{manual_id}/roles/{role_id}/post.test.ts
+++ src/routes/manuals/{manual_id}/roles/{role_id}/post.test.ts
@@ -1,3 +1,13 @@
+import { getMockRes } from "@jest-mock/express";
+import postController from "./post";
+import { Request } from "express";
+
+const { res, mockClear } = getMockRes();
+
+beforeEach(mockClear);
+
 describe("", () => {
-    test.todo("");
+    test("", async () => {
+        await postController({} as Request, res);
+    });
 });
--- src/util/permalink.ts
+++ src/util/permalink.ts
@@ -1,4 +1,3 @@
-import Logs from "@noahvarghese/logger";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,17 +6,16 @@ const { TARGET_ENV } = process.env;
 let clientURL: string = process.env[`ENV_${TARGET_ENV}_CLIENT`] ?? "";
 let serverURL: string = process.env[`ENV_${TARGET_ENV}_SERVER`] ?? "";
 
-if (clientURL !== "") {
-    clientURL = `http${TARGET_ENV !== "LOCAL" ? "s" : ""}://${clientURL}/`;
-} else {
-    Logs.Error(`No client origin found for environment ${TARGET_ENV}`);
+if (clientURL === "") {
+    throw new Error(`No client origin found for environment ${TARGET_ENV}`);
 }
 
-if (serverURL !== "") {
-    serverURL = `http${TARGET_ENV !== "LOCAL" ? "s" : ""}://${serverURL}/`;
-} else {
-    Logs.Error(`No server origin found for environment ${TARGET_ENV}`);
+if (serverURL === "") {
+    throw new Error(`No server origin found for environment ${TARGET_ENV}`);
 }
+
+clientURL = `http${TARGET_ENV !== "LOCAL" ? "s" : ""}://${clientURL}/`;
+serverURL = `http${TARGET_ENV !== "LOCAL" ? "s" : ""}://${serverURL}/`;
 
 export const client = (path = ""): string =>
     clientURL + (path[0] === "/" ? path.substring(1) : path);
