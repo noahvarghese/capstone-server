41de058dc1f2172cc118afb845e1d51c98f78dbb
noahvarghese <varghese.noah@gmail.com>
2022-02-15 17:45:21 +0000

    added delete route, and hooked up post route

--- src/routes/manuals/{manual_id}/roles/{role_id}/index.ts
+++ src/routes/manuals/{manual_id}/roles/{role_id}/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import deleteController from "./delete";
+import postController from "./post";
 
 const router = Router();
+
+router.post("/", postController);
+router.delete("/", deleteController);
 
 export default router;
