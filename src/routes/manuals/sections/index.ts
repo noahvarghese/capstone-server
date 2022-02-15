44638ff80ef10b07eddefc7ddc880dfee4296ceb
noahvarghese <varghese.noah@gmail.com>
2022-02-15 20:26:00 +0000

    added policies route

--- src/routes/manuals/sections/index.ts
+++ src/routes/manuals/sections/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import idRouter from "./{id}";
+import policyRouter from "./policies";
 
 const router = Router();
 
 router.use("/:id", idRouter);
+router.use("/policies", policyRouter);
 
 export default router;
