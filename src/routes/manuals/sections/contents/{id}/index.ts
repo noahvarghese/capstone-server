db5cd5b3770b30c41871ef827986d6129cc437ce
noahvarghese <varghese.noah@gmail.com>
2022-02-16 14:15:58 +0000

    Added read route

--- src/routes/manuals/sections/contents/{id}/index.ts
+++ src/routes/manuals/sections/contents/{id}/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import readRouter from "./read";
 import deleteController from "./delete";
 import getController from "./get";
 import putController from "./put";
 
 const router = Router();
+
+router.use("/read", readRouter);
 
 router.get("/", getController);
 router.put("/", putController);
