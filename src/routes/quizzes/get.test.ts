703b64a20f164f3c489ea105ea34d88ce686db0c
noahvarghese <varghese.noah@gmail.com>
2022-02-17 17:16:05 +0000

    added get all quizzes route

--- src/routes/quizzes/get.test.ts
+++ src/routes/quizzes/get.test.ts
@@ -1,3 +1,487 @@
-describe("", () => {
-    test.todo("");
+import { getMockRes } from "@jest-mock/express";
+import Role, { AccessKey } from "@models/role";
+import DBConnection from "@test/support/db_connection";
+import { setupAdmin } from "@test/unit/setup";
+import { unitTeardown } from "@test/unit/teardown";
+import { SessionData } from "express-session";
+import { Connection } from "typeorm";
+import getController from "./get";
+import { Request, Response } from "express";
+import { deepClone } from "@util/obj";
+import Manual from "@models/manual/manual";
+import ManualAssignment from "@models/manual/assignment";
+import Quiz from "@models/quiz/quiz";
+import Department from "@models/department";
+
+const { mockClear, res } = getMockRes();
+
+beforeEach(mockClear);
+
+let business_id: number,
+    user_id: number,
+    role_id: number,
+    department_id: number,
+    manual_id: number;
+const quiz_ids: number[] = [];
+let conn: Connection;
+let session: Omit<SessionData, "cookie">;
+
+beforeAll(async () => {
+    await DBConnection.init();
+    conn = await DBConnection.get();
+
+    ({ business_id, user_id } = await setupAdmin(conn));
+
+    session = {
+        user_id,
+        business_ids: [business_id],
+        current_business_id: business_id,
+    };
+
+    ({
+        identifiers: [{ id: manual_id }],
+    } = await conn.manager.insert(
+        Manual,
+        new Manual({
+            title: "TEST",
+            prevent_delete: false,
+            prevent_edit: false,
+            published: true,
+            updated_by_user_id: user_id,
+            business_id,
+        })
+    ));
+    let {
+        identifiers: [{ id }],
+    } = await conn.manager.insert(
+        Quiz,
+        new Quiz({
+            title: "TEST",
+            prevent_delete: false,
+            prevent_edit: false,
+            published: true,
+            max_attempts: 1,
+            manual_id,
+            updated_by_user_id: user_id,
+        })
+    );
+
+    quiz_ids.push(id);
+    ({
+        identifiers: [{ id }],
+    } = await conn.manager.insert(
+        Quiz,
+        new Quiz({
+            title: "TEST123",
+            prevent_delete: false,
+            prevent_edit: false,
+            published: true,
+            max_attempts: 1,
+            manual_id,
+            updated_by_user_id: user_id,
+        })
+    ));
+
+    quiz_ids.push(id);
+
+    role_id = (await conn.manager.findOneOrFail(Role)).id;
+    department_id = (await conn.manager.findOneOrFail(Department)).id;
+
+    await conn.manager.insert(
+        ManualAssignment,
+        new ManualAssignment({
+            manual_id,
+            updated_by_user_id: user_id,
+            role_id,
+        })
+    );
+});
+
+afterAll(async () => {
+    await conn.manager.delete(Manual, () => "");
+    await unitTeardown(conn);
+    await DBConnection.close();
+});
+
+describe("sort", () => {
+    const cases = [
+        { sort_field: "title", sort_order: "ASC" },
+        { sort_field: "title", sort_order: "DESC" },
+    ];
+
+    test.each(cases)(
+        "given sort field %p and sort order %p, the results will match",
+        async ({ sort_field, sort_order }) => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            let data: { [x: string]: any }[] = [];
+
+            await getController(
+                {
+                    session,
+                    query: { sort_field, sort_order },
+                    dbConnection: conn,
+                } as unknown as Request,
+                {
+                    status: () => {
+                        return {
+                            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                            send: (u: { [x: string]: any }[]) => {
+                                data = u;
+                            },
+                        };
+                    },
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    send: (u: { [x: string]: any }[]) => {
+                        data = u;
+                    },
+                } as unknown as Response
+            );
+            expect(data.length).toBeGreaterThan(1);
+            const dataCopy = deepClone(data);
+            expect(dataCopy).toBeInstanceOf(Array);
+            const sortedData = dataCopy.sort((a, b): number => {
+                const aVal = JSON.stringify(a[sort_field]);
+                const bVal = JSON.stringify(b[sort_field]);
+
+                if (sort_order === "ASC") {
+                    return aVal < bVal ? -1 : aVal === bVal ? 0 : 1;
+                } else {
+                    return aVal < bVal ? 1 : aVal === bVal ? 0 : -1;
+                }
+            });
+
+            expect(JSON.stringify(data)).toBe(JSON.stringify(sortedData));
+        }
+    );
+});
+
+describe("search", () => {
+    const cases = [
+        {
+            search: "123",
+        },
+    ];
+
+    test.each(cases)("%p", async ({ search }) => {
+        let status;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let send: any;
+        await getController(
+            {
+                query: { search },
+                session,
+                dbConnection: conn,
+            } as unknown as Request,
+            {
+                send: (v: unknown) => {
+                    send = v;
+                },
+                status: (v: unknown) => {
+                    status = v;
+                    return {
+                        send: (v: unknown) => {
+                            send = v;
+                        },
+                    };
+                },
+            } as Response
+        );
+        expect(status).toBe(200);
+        expect(send.length).toBe(1);
+        expect((send[0].title as string).toLowerCase()).toContain(
+            search.toLowerCase()
+        );
+    });
+});
+
+describe("pagination", () => {
+    describe("page", () => {
+        const cases = [
+            { limit: 1, page: 1 },
+            { limit: 1, page: 2 },
+        ];
+        test.each(cases)("%p", async ({ limit, page }) => {
+            const conn = await DBConnection.get();
+            await getController(
+                {
+                    query: { limit, page },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith(
+                expect.objectContaining({ length: 1 })
+            );
+        });
+    });
+
+    describe("limit", () => {
+        const cases = [
+            { limit: 1, page: 1 },
+            { limit: 2, page: 1 },
+        ];
+        test.each(cases)("%p", async ({ limit, page }) => {
+            const conn = await DBConnection.get();
+            let status;
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            let send: any;
+            await getController(
+                {
+                    query: { limit, page },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                {
+                    send: (v: unknown) => {
+                        send = v;
+                    },
+                    status: (v: unknown) => {
+                        status = v;
+                        return {
+                            send: (v: unknown) => {
+                                send = v;
+                            },
+                        };
+                    },
+                } as Response
+            );
+            expect(status).toBe(200);
+            expect(send.length).toBe(limit);
+        });
+    });
+});
+
+describe("filter", () => {
+    const cases = [
+        { id: department_id, field: "department" },
+        { id: role_id, field: "role" },
+        { id: manual_id, field: "manual" },
+    ];
+    test.each(cases)("%p", async ({ id, field }) => {
+        await getController(
+            {
+                session,
+                query: {
+                    filter_field: field,
+                    filter_ids: JSON.stringify([id + 1]),
+                },
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+        expect(res.send).toHaveBeenCalledWith(
+            // FIXME: Not sure why it returns an empty array
+            expect.objectContaining({ length: 0 })
+        );
+    });
+});
+
+describe("user not assigned", () => {
+    beforeAll(async () => {
+        await conn.manager.delete(ManualAssignment, {
+            manual_id,
+            role_id,
+        });
+        await conn.manager.update(Role, role_id, {
+            access: "USER",
+            prevent_edit: false,
+        });
+    });
+
+    afterAll(async () => {
+        await conn.manager.insert(ManualAssignment, {
+            manual_id,
+            role_id,
+            updated_by_user_id: user_id,
+        });
+        await conn.manager.update(Role, role_id, {
+            access: "ADMIN",
+            prevent_edit: true,
+        });
+    });
+
+    test("", async () => {
+        await getController(
+            {
+                session,
+                query: {},
+                dbConnection: conn,
+            } as unknown as Request,
+            res
+        );
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith(
+            expect.objectContaining({ length: 0 })
+        );
+    });
+});
+
+describe("manual published", () => {
+    const mPublished = [true, false];
+    describe.each(mPublished)("%p", (mP) => {
+        const qPublished = [true, false];
+        beforeAll(async () => {
+            await conn.manager.update(Manual, manual_id, { published: mP });
+        });
+
+        describe.each(qPublished)("%p", (p) => {
+            const permissions = ["ADMIN", "MANAGER", "USER"];
+            beforeAll(async () => {
+                Promise.all(
+                    quiz_ids.map((q) =>
+                        conn.manager.update(Quiz, q, {
+                            published: p,
+                        })
+                    )
+                );
+            });
+            describe.each(permissions)("permissions %p", (access) => {
+                beforeAll(async () => {
+                    await conn.manager.update(Role, role_id, {
+                        access: access as AccessKey,
+                        prevent_edit: false,
+                    });
+                });
+                afterAll(async () => {
+                    await conn.manager.update(Role, role_id, {
+                        access: "ADMIN",
+                        prevent_edit: true,
+                    });
+                });
+
+                test("", async () => {
+                    await getController(
+                        {
+                            session,
+                            query: {},
+                            dbConnection: conn,
+                        } as unknown as Request,
+                        res
+                    );
+
+                    expect(res.status).toHaveBeenCalledWith(200);
+
+                    if (access === "USER" && (p === false || mP === false)) {
+                        expect(res.send).toHaveBeenCalledWith(
+                            expect.objectContaining({ length: 0 })
+                        );
+                    } else {
+                        expect(res.send).toHaveBeenCalledWith(
+                            expect.objectContaining({ length: 2 })
+                        );
+                    }
+                });
+            });
+        });
+    });
+});
+
+describe("invalid query params", () => {
+    describe("pagination", () => {
+        const cases = [
+            { limit: "yolo", page: 1 },
+            { limit: " ", page: 1 },
+            { limit: false, page: 1 },
+            { limit: true, page: 1 },
+            { limit: {}, page: 1 },
+            { limit: { test: "" }, page: 1 },
+            { limit: "", page: 1 },
+            { limit: undefined, page: 1 },
+            { limit: null, page: 1 },
+            { page: "yolo", limit: 1 },
+            { page: " ", limit: 1 },
+            { page: false, limit: 1 },
+            { page: true, limit: 1 },
+            { page: {}, limit: 1 },
+            { page: { test: "" }, limit: 1 },
+            { page: "", limit: 1 },
+            { page: undefined, limit: 1 },
+            { page: null, limit: 1 },
+            { limit: 1 },
+            { page: 1 },
+        ];
+
+        test.each(cases)("%p", async ({ limit, page }) => {
+            const conn = await DBConnection.get();
+            await getController(
+                {
+                    query: { limit, page },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.send).toHaveBeenCalledWith("Invalid pagination options");
+        });
+    });
+
+    describe("filter", () => {
+        const cases = [
+            { filter_field: "", filter_ids: "[]" },
+            { filter_field: "role", filter_ids: "[]" },
+            { filter_field: "department", filter_ids: "[]" },
+            { filter_field: "test", filter_ids: "[1]" },
+            { filter_field: undefined, filter_ids: "[1]" },
+            { filter_field: null, filter_ids: "[1]" },
+            { filter_field: {}, filter_ids: "[1]" },
+            { filter_field: { test: "" }, filter_ids: "[1]" },
+            { filter_field: 1, filter_ids: "[1]" },
+            { filter_field: NaN, filter_ids: "[1]" },
+            { filter_field: " ", filter_ids: "[1]" },
+            { filter_field: "role", filter_ids: "" },
+            { filter_field: "role", filter_ids: null },
+            { filter_field: "role", filter_ids: undefined },
+            { filter_field: "role", filter_ids: " " },
+            { filter_field: "department", filter_ids: {} },
+            {
+                filter_field: "department",
+                filter_ids: { test: "" },
+            },
+        ];
+        test.each(cases)("%p", async ({ filter_field, filter_ids }) => {
+            const conn = await DBConnection.get();
+            await getController(
+                {
+                    query: { filter_field, filter_ids },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.send).toHaveBeenCalledWith("Invalid filter options");
+        });
+    });
+
+    describe("sort", () => {
+        const cases = [
+            { sort_field: "", sort_order: "ASC" },
+            { sort_field: "", sort_order: "DESC" },
+            { sort_field: "", sort_order: "YOLO" },
+            { sort_field: "first_name", sort_order: "" },
+            { sort_field: "last_name", sort_order: " " },
+            { sort_field: "email", sort_order: 1 },
+            { sort_field: "phone", sort_order: NaN },
+            { sort_field: "department", sort_order: null },
+            { sort_field: "role", sort_order: undefined },
+            { sort_field: "yolo", sort_order: "ASC" },
+        ];
+        test.each(cases)("%p", async ({ sort_field, sort_order }) => {
+            const conn = await DBConnection.get();
+            await getController(
+                {
+                    query: { sort_field, sort_order },
+                    session,
+                    dbConnection: conn,
+                } as unknown as Request,
+                res
+            );
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.send).toHaveBeenCalledWith("Invalid sort options");
+        });
+    });
 });
--- src/routes/quizzes/get.ts
+++ src/routes/quizzes/get.ts
@@ -1,10 +1,153 @@
-import Logs from "@noahvarghese/logger";
+import Department from "@models/department";
+import ManualAssignment from "@models/manual/assignment";
+import Manual from "@models/manual/manual";
+import Quiz from "@models/quiz/quiz";
+import Role from "@models/role";
+import User from "@models/user/user";
+import UserRole from "@models/user/user_role";
+import isNumber from "@noahvarghese/get_j_opts/build/lib/isNumber";
+import { isJson } from "@util/obj";
 import { Request, Response } from "express";
 
+const filterFields = ["department", "role", "manual"] as const;
+type FilterKey = typeof filterFields[number];
+
+const sortFields = ["title"] as const;
+type SortFieldKey = typeof sortFields[number];
+
+const sortOrders = ["ASC", "DESC"] as const;
+type SortOrderKey = typeof sortOrders[number];
+
 const getController = async (req: Request, res: Response): Promise<void> => {
-    Logs.Debug(req);
-    res.sendStatus(200);
-    return;
+    const {
+        query: {
+            filter_ids: filterIds,
+            filter_field,
+            search,
+            sort_order,
+            sort_field,
+            limit,
+            page,
+        },
+        session: { user_id, current_business_id },
+        dbConnection,
+    } = req;
+
+    // 1.   Check that url query items are correct
+    const filter_ids = isJson(filterIds as string)
+        ? JSON.parse(filterIds as string)
+        : null;
+
+    if ((filter_ids && !filter_field) || (!filter_ids && filter_field)) {
+        res.status(400).send("Invalid filter options");
+        return;
+    }
+
+    if (filter_ids) {
+        if (
+            !Array.isArray(filter_ids) ||
+            (filter_ids as string[]).reduce((p, c) => {
+                return p && isNaN(Number(c));
+            }, true as boolean)
+        ) {
+            res.status(400).send("Invalid filter options");
+            return;
+        }
+    }
+
+    if (filter_field && !filterFields.includes(filter_field as FilterKey)) {
+        res.status(400).send("Invalid filter options");
+        return;
+    }
+
+    if ((sort_order && !sort_field) || (!sort_order && sort_field)) {
+        res.status(400).send("Invalid sort options");
+        return;
+    }
+
+    if (sort_order && !sortOrders.includes(sort_order as SortOrderKey)) {
+        res.status(400).send("Invalid sort options");
+        return;
+    }
+
+    if (sort_field && !sortFields.includes(sort_field as SortFieldKey)) {
+        res.status(400).send("Invalid sort options");
+        return;
+    }
+
+    if ((limit && !page) || (!limit && page)) {
+        res.status(400).send("Invalid pagination options");
+        return;
+    }
+
+    if (limit && !isNumber(limit)) {
+        res.status(400).send("Invalid pagination options");
+        return;
+    }
+
+    if (page && !isNumber(page)) {
+        res.status(400).send("Invalid pagination options");
+        return;
+    }
+
+    // 2.   Check permissions
+    const [isAdmin, isManager] = await Promise.all([
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isAdmin(dbConnection, current_business_id!, user_id!),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isManager(dbConnection, current_business_id!, user_id!),
+    ]);
+
+    //  3.  Perform action
+    let query = dbConnection
+        .createQueryBuilder()
+        .select("q.id, q.title, q.published, q.prevent_delete, q.prevent_edit")
+        .from(Quiz, "q")
+        .leftJoin(Manual, "m", "m.id = q.manual_id")
+        // Joins are for filtering
+        .leftJoin(ManualAssignment, "ma", "ma.manual_id = m.id")
+        .leftJoin(UserRole, "ur", "ur.role_id = ma.role_id")
+        .leftJoin(Role, "r", "r.id = ma.role_id")
+        .leftJoin(Department, "d", "d.id = r.department_id")
+        .where("m.business_id = :current_business_id", {
+            current_business_id,
+        });
+
+    if (!(isAdmin || isManager)) {
+        query = query
+            .andWhere("m.published = :manual_published", {
+                manual_published: true,
+            })
+            .andWhere("q.published = :quiz_published", { quiz_published: true })
+            .andWhere("ur.user_id = :user_id", { user_id });
+    }
+
+    if (filter_field && filter_ids) {
+        query = query.andWhere(
+            `${(filter_field as string)[0]}.id IN (:...ids)`,
+            { ids: filter_ids }
+        );
+    }
+
+    if (search) {
+        const sqlizedSearchItem = `%${search}%`;
+        query = query.andWhere("q.title like :quiz", {
+            quiz: sqlizedSearchItem,
+        });
+    }
+
+    query = query.orderBy(
+        sort_field ? `q.${sort_field}` : "q.created_on",
+        (sort_order as SortOrderKey) ?? "DESC"
+    );
+
+    if (page && limit) {
+        query = query
+            .limit(Number(limit))
+            .offset(Number(page) * Number(limit) - Number(limit));
+    }
+
+    res.status(200).send(await query.getRawMany());
 };
 
 export default getController;
