b488975aa521200e53f04e22358be72db67839b1
noahvarghese <varghese.noah@gmail.com>
2022-02-17 16:01:44 +0000

    added id router

--- src/routes/quizzes/index.ts
+++ src/routes/quizzes/index.ts
@@ -1,8 +1,11 @@
 import authenticated from "@middleware/authenticated";
 import { Router } from "express";
+import idRouter from "./{id}";
 
 const router = Router();
 
 router.use(authenticated);
+
+router.use("/:id", idRouter);
 
 export default router;
