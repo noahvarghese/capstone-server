5d3129db2054f51a4b72132ceadc6c2ec0b3ed5d
noahvarghese <varghese.noah@gmail.com>
2022-02-17 16:35:49 +0000

    hooked up quizzes

--- src/routes/quizzes/index.ts
+++ src/routes/quizzes/index.ts
@@ -1,5 +1,6 @@
 import authenticated from "@middleware/authenticated";
 import { Router } from "express";
+import getController from "./get";
 import idRouter from "./{id}";
 
 const router = Router();
@@ -7,5 +8,7 @@ const router = Router();
 router.use(authenticated);
 
 router.use("/:id", idRouter);
+
+router.get("/", getController);
 
 export default router;
