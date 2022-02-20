98ee71582456fcc8130a47d68eb3f86d5d1c92ed
noahvarghese <varghese.noah@gmail.com>
2022-02-20 16:22:01 +0000

    added get individual quiz question

--- src/routes/quizzes/sections/questions/index.ts
+++ src/routes/quizzes/sections/questions/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import idRouter from "./{id}";
-import questionRouter from "./questions";
 
 const router = Router();
 
 router.use("/:id", idRouter);
-router.use("/questions", questionRouter);
 
 export default router;
