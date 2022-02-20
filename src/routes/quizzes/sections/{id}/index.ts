6ca7464603677d097990c77c74332a9ab3ec7741
noahvarghese <varghese.noah@gmail.com>
2022-02-20 15:11:40 +0000

    joined routes

--- src/routes/quizzes/sections/{id}/index.ts
+++ src/routes/quizzes/sections/{id}/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import deleteController from "./delete";
 import getController from "./get";
+import putController from "./put";
 
 const router = Router();
 
 router.get("/", getController);
+router.put("/", putController);
+router.delete("/", deleteController);
 
 export default router;
