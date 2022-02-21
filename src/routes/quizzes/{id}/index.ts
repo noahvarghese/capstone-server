9d4b31c17b65cd6eff5102ae2f97a6f79503068b
noahvarghese <varghese.noah@gmail.com>
2022-02-21 22:00:22 +0000

    added attempts router

--- src/routes/quizzes/{id}/index.ts
+++ src/routes/quizzes/{id}/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import sectionRouter from "./sections";
+import attemptRouter from "./attempts";
 import deleteController from "./delete";
 import getController from "./get";
 import putController from "./put";
@@ -7,6 +8,7 @@ import putController from "./put";
 const router = Router();
 
 router.use("/sections", sectionRouter);
+router.use("/attempts", attemptRouter);
 
 router.get("/", getController);
 router.put("/", putController);
