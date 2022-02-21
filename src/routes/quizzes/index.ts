f059126a2ee2d48301ca0a747a90ef1834986e15
noahvarghese <varghese.noah@gmail.com>
2022-02-21 23:30:07 +0000

    added attempt router

--- src/routes/quizzes/index.ts
+++ src/routes/quizzes/index.ts
@@ -2,12 +2,14 @@ import authenticated from "@middleware/authenticated";
 import { Router } from "express";
 import getController from "./get";
 import idRouter from "./{id}";
+import attemptRouter from "./attempts";
 
 const router = Router();
 
 router.use(authenticated);
 
 router.use("/:id", idRouter);
+router.use("/attempts", attemptRouter);
 
 router.get("/", getController);
 
