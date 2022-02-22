c788e1207d385bfa1b2ad251fa0cf08a89b1e8c8
noahvarghese <varghese.noah@gmail.com>
2022-02-22 00:20:43 +0000

    added delete quiz attempt

---
+++ src/routes/quizzes/attempts/{id}/delete.ts
@@ -0,0 +1,30 @@
+import QuizAttempt from "@models/quiz/attempt";
+import User from "@models/user/user";
+import { Request, Response } from "express";
+
+const deleteController = async (req: Request, res: Response): Promise<void> => {
+    const {
+        dbConnection,
+        session: { user_id, current_business_id },
+        params: { id },
+    } = req;
+
+    const [isAdmin, isManager] = await Promise.all([
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isAdmin(dbConnection, current_business_id!, user_id!),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        User.isManager(dbConnection, current_business_id!, user_id!),
+    ]);
+
+    if (!(isAdmin || isManager)) {
+        res.sendStatus(403);
+        return;
+    }
+
+    await dbConnection.manager.delete(QuizAttempt, id);
+
+    res.sendStatus(200);
+    return;
+};
+
+export default deleteController;
