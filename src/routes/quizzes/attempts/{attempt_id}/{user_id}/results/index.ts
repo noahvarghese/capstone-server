189410d06b42a35b353e65e81507e6e042124369
noahvarghese <varghese.noah@gmail.com>
2022-02-22 02:26:03 +0000

    added get all results for attempt

--- src/routes/quizzes/attempts/{attempt_id}/{user_id}/results/index.ts
+++ src/routes/quizzes/attempts/{attempt_id}/{user_id}/results/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import getController from "./get";
 import resultIdRouter from "./{result_id}";
 
 const router = Router();
 
 router.use("/:result_id", resultIdRouter);
+
+router.get("/", getController);
 
 export default router;
