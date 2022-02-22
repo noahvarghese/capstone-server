a64d5162e0118f1e247cf28f5aae0b9c232ecad3
noahvarghese <varghese.noah@gmail.com>
2022-02-22 05:06:05 +0000

    added routers for incomplete manuals

--- src/routes/reports/index.ts
+++ src/routes/reports/index.ts
@@ -1,11 +1,13 @@
 import authenticated from "@middleware/authenticated";
 import { Router } from "express";
 import adminRouter from "./admin";
+import manualRouter from "./manuals";
 
 const router = Router();
 
 router.use(authenticated);
 
 router.use("/admin", adminRouter);
+router.use("/manuals", manualRouter);
 
 export default router;
