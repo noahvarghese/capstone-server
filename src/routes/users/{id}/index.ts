15e2c3879cf69357294f25a175308e8f16cd49d4
noahvarghese <varghese.noah@gmail.com>
2022-02-22 05:02:48 +0000

    moved authenticated to the second highest layer

--- src/routes/users/{id}/index.ts
+++ src/routes/users/{id}/index.ts
@@ -1,11 +1,9 @@
-import authenticated from "@middleware/authenticated";
 import { Router } from "express";
 import { getUserController } from "./get";
 import { updateUserController } from "./put";
 
 const router = Router();
 
-router.use(authenticated);
 router.put("/", updateUserController);
 router.get("/", getUserController);
 
