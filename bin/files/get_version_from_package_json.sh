bb833e48c42d7b5e577233c61c6b7880e04b0435
noahvarghese <varghese.noah@gmail.com>
2022-02-24 20:05:39 +0000

    do not auto incrmemnet package version

--- bin/files/get_version_from_package_json.sh
+++ bin/files/get_version_from_package_json.sh
@@ -4,8 +4,6 @@
 VERSION=$(cat ./package.json | grep \"version\" | cut -d : -f 2 | sed 's/\",$//g' | sed 's/\"//g') 
 # Gets last digit of version number
 TRAILING_VERSION=$(echo $VERSION | cut -d . -f 3)
-# Adds 1 to last digit of version number
-TRAILING_VERSION=$((TRAILING_VERSION+1))
 VERSION=$(echo $VERSION | cut -d . -f 1).$(echo $VERSION | cut -d . -f 2).$TRAILING_VERSION
 
 echo $VERSION
