80a1a81fbe1a7b4760db364c8bc9b7d60106a0c3
noahvarghese <varghese.noah@gmail.com>
2022-03-02 02:15:20 +0000

    fixed auth middleware intercepting request

--- src/routes/members/index.ts
+++ src/routes/members/index.ts
@@ -6,9 +6,11 @@ import authenticated from "@middleware/authenticated";
 
 const router = Router({ mergeParams: true });
 
+// authenticated performed at the method level within invite
+// Must be above using the auth middleware
+router.use("/invite", inviteRouter);
+
 router.get("/", authenticated, getController);
 router.use("/:user_id", authenticated, idRouter);
-// authenticated performed at the method level within invite
-router.use("/invite", inviteRouter);
 
 export default router;
